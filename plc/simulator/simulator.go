cc93d612979098c8e733ea1b594d2933471dd984
Shubham <shubham.basu@joshsoftware.com>
2020-05-27 15:24:28 +0530

    Update simulator.go
    Added dummy scan values in Monitor return

--- plc/simulator/simulator.go
+++ plc/simulator/simulator.go
@@ -1,6 +1,10 @@
 package simulator
 
-import "mylab/cpagent/plc"
+import (
+	"mylab/cpagent/plc"
+
+	logger "github.com/sirupsen/logrus"
+)
 
 type Simulator struct {
 	ExitCh chan error
@@ -11,6 +15,7 @@ func NewSimulator(exit chan error) plc.Driver {
 }
 
 func (d *Simulator) HeartBeat() {
+	logger.Info("Starting HeartBeat...")
 	return
 }
 
@@ -20,5 +25,36 @@ func (d *Simulator) ConfigureRun(plc.Stage) error {
 
 // Monitor periodically. If Status=CYCLE_COMPLETE, the Scan will be populated
 func (d *Simulator) Monitor(cycle uint16) (scan plc.Scan, err error) {
+	scan.Cycle = cycle + 1 // for testing, can be changed as required
+
+	scan.Temp = 150 // for testing, can be changed to 0 for setting CycleComplete to false
+
+	scan.LidTemp = 150
+
+	// Read current cycle status
+	if scan.Temp == 0 {
+		scan.CycleComplete = false
+		return
+	}
+	scan.CycleComplete = true
+
+	// If the invoker has already read this cycle data, don't send it again!
+	if cycle == scan.Cycle {
+		return
+	}
+
+	// Scan all the data from the Wells (96 x 6).
+	for i := 0; i < 6; i++ {
+		for j := 0; j < 16; j++ {
+			emission := plc.Emissions{}
+
+			for k := 0; k < 6; k++ {
+				emission[k] = uint16(256 + k) //256 257 258 259 260 261
+			}
+
+			scan.Wells[(i*16)+j] = emission
+		}
+	}
+
 	return
 }
