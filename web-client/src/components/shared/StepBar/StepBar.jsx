97c1262ac5e1f4aa59f6add36d2b33eefe57891d
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-06-07 21:48:28 +0530

    StepBar component ui

---
+++ web-client/src/components/shared/StepBar/StepBar.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { Nav, NavItem } from "reactstrap";
+import { NavLink } from "react-router-dom";
+
+export const Step = styled(Nav)`
+  margin: 0 0 40px;
+  padding: 0 56px;
+`;
+
+export const StepItem = styled(NavItem)`
+  width: 200px;
+  height: 40px;
+  margin: 0 8px 0 0;
+  text-align: center;
+`;
+
+export const StepLink = styled(NavLink).attrs({
+  className: "nav-link",
+})`
+  background-color: white;
+  font-size: 16px;
+  line-height: 24px;
+  color: #707070;
+
+  &:hover,
+  &:focus {
+    color: #707070;
+  }
+
+  &.is-disabled {
+    opacity: 0.53;
+    pointer-events: none;
+  }
+`;
--- web-client/src/containers/TemplateContainer.jsx
+++ web-client/src/containers/TemplateContainer.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Step, StepItem, StepLink } from "components/shared/StepBar/StepBar";
+import MlCard from 'components/shared/Card/Card';
 
 
 const TemplateContainer = props => {
-  return <div>templates</div>;
+  return (
+		<>
+			<Step>
+				<StepItem>
+					<StepLink to="/templates">Template</StepLink>
+				</StepItem>
+				<StepItem>
+					<StepLink to="/login" className="is-disabled">View Target</StepLink>
+				</StepItem>
+			</Step>
+      <MlCard></MlCard>
+		</>
+	);
 };
 
 TemplateContainer.propTypes = {};
