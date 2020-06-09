b9924ef7258a4f5cae0b376ab72121dc8230c8a3
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-06-09 21:41:54 +0530

    Icon font, shared components and other ui changes

--- web-client/src/core-components/Button/index.jsx
+++ web-client/src/core-components/Button/index.jsx
@@ -10,44 +10,6 @@ const StyledButton = styled(Button)`
 	padding: 10px 20px;
 	border-radius: 27px;
 	box-shadow: 0 2px 6px #00000029;
-
-	${(props) =>
-		props.icon &&
-		css`
-			width: 42px;
-			height: 42px;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			background-color: transparent;
-			color: white !important;
-			font-weight: normal;
-			padding: 4px;
-			border: 1px solid white;
-			border-radius: 50%;
-
-			&:not(:disabled):not(.disabled):active,
-			&:not(:disabled):not(.disabled).active,
-			&:focus,
-			&.focus,
-			&:hover {
-				background-color: transparent;
-				border: 1px solid white;
-			}
-
-			&:not(:disabled):not(.disabled):active:focus,
-			&:not(:disabled):not(.disabled).active:focus {
-				box-shadow: 0 2px 6px #00000029;
-			}
-		`};
-
-	&.btn-primary {
-		color: white;
-	}
-
-	&.btn-secondary {
-		color: #666666;
-	}
 `;
 
 export default StyledButton;
--- web-client/src/core-components/Checkbox/index.jsx
+++ web-client/src/core-components/Checkbox/index.jsx
@@ -6,36 +6,11 @@ const StyledCheckBox = styled(CustomInput)`
 	padding-left: ${(props) => (props.label ? "40px" : "24px")};
 
 	.custom-control-input:checked ~ .custom-control-label::after {
-		background-image: none;
-		background-color: #aedbd5;
 		left: ${(props) => (props.label ? "-36px" : "-20px")};
 	}
 
-	.custom-control-input:focus ~ .custom-control-label::before {
-		box-shadow: none;
-		border-color: #999999;
-	}
-
-	.custom-control-label {
-		&::before {
-			width: 24px;
-			height: 24px;
-			background-color: white;
-			border: 1px solid #999999;
-			top: 0;
-			left: ${(props) => (props.label ? "-40px" : "-24px")};
-			border-radius: 4px;
-		}
-
-		&::after {
-			border-radius: 2px;
-		}
-	}
-
-	.custom-control-input:checked ~ .custom-control-label::before,
-	.custom-control-input:not(:disabled):active ~ .custom-control-label::before {
-		background-color: white;
-		border-color: #999999;
+	.custom-control-label::before {
+		left: ${(props) => (props.label ? "-40px" : "-24px")};
 	}
 `;
 
