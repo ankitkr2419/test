904cd212d0e0e2b3a2017279a027fefc0acd0cd3
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-06-11 20:06:51 +0530

    Template screens changes

--- web-client/src/containers/ActivityContainer.jsx
+++ web-client/src/containers/ActivityContainer.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ActivityContainer = (props) => {
 	return (
-		<>
-			<div className="d-flex align-items-center justify-content-center h-100">
-				<h4 className="text-center text-uppercase pb-5 mb-5">Activity Log</h4>
+		<div className="activity-content h-100">
+			<div className="d-flex align-items-center justify-content-center">
+				<h4 className="text-center text-uppercase py-5">Activity Log</h4>
 			</div>
-		</>
+		</div>
 	);
 };
 
