47cb3315a4cdbd37f51e1eb0558a323289d3b3ec
Unknown <sw09007@gmail.com>
2020-06-17 19:38:07 +0530

    unique constraints added
    unique constraints added

--- migrations/1591331125_create_template_targets_table.up.sql
+++ migrations/1591331125_create_template_targets_table.up.sql
@@ -3,4 +3,4 @@ CREATE TABLE template_targets
     template_id uuid,
     target_id uuid,
     threshold float,
-    CONSTRAINT unqexp UNIQUE (template_id, target_id));
+    CONSTRAINT unq UNIQUE (template_id, target_id));
--- migrations/1592376750_create_exp_temp_targets_table.up.sql
+++ migrations/1592376750_create_exp_temp_targets_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE experiment_template_targets
     experiment_id uuid,
     template_id uuid,
     target_id uuid,
-    threshold float
-    );
+    threshold float,
+    CONSTRAINT unqexp UNIQUE (template_id, target_id));
