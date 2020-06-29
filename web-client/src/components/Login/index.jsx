2dabfead5a2f17adc4e858834c5d4d557a1d3b5b
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-06-29 21:50:12 +0530

    Login screen changes

--- web-client/src/components/Login/index.jsx
+++ web-client/src/components/Login/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardBody, Button } from 'core-components';
-import { ButtonGroup } from 'shared-components';
+import { ButtonGroup, Text } from 'shared-components';
 import LoginForm from './LoginForm';
 
 const LoginComponent = (props) => {
@@ -17,44 +17,35 @@ const LoginComponent = (props) => {
 	const [password, setPassword] = useState('');
 
 	return (
-		<div className="login-content">
+		<div className='login-content'>
 			<ButtonGroup>
 				<Button
-					color="secondary"
+					color='secondary'
 					onClick={() => {
 						setIsAdminFormVisibility(true);
 					}}
-					className="mr-4"
+					className='mr-4'
 					active={isAdminFormVisible}
 				>
-          Admin
+					Admin
 				</Button>
-				<Button color="secondary" className="mr-4">
-          Supervisor
+				<Button color='secondary' className='mr-4'>
+					Supervisor
 				</Button>
 			</ButtonGroup>
-			<Card className="card-login">
-				<CardBody className="d-flex scroll-y">
-					<div className="flex-100">
-						<h1 className="card-title">Compact 32</h1>
-						{isAdminFormVisible === false && (
-							<Button
-								onClick={loginAsOperator}
-								color="primary"
-								className="mx-auto"
+			<Card className='card-login'>
+				<CardBody className='d-flex flex-column px-5 py-4 scroll-y'>
+					<div className='d-flex align-items-center mb-3 pt-3'>
+						<h1 className='text-default font-weight-normal mb-0 px-3'>
+							Compact 32
+						</h1>
+						{isAdminFormVisible && (
+							<Text
+								tag='h6'
+								className='flex-50 text-default text-center font-weight-bold mb-0 ml-auto px-3'
 							>
-                Login as Operator
-							</Button>
-						)}
-						{isAdminFormVisible === true && (
-							<Button
-								onClick={() => {
-									setIsAdminFormVisibility(false);
-								}}
-								color="secondary"
-							>
-                Back
-							</Button>
+								Admin Login
+							</Text>
 						)}
 					</div>
 					{isAdminFormVisible && (
@@ -67,6 +58,23 @@ const LoginComponent = (props) => {
 							isLoginError={isLoginError}
 						/>
 					)}
+					<div className='mt-auto mb-5 pb-4'>
+						{isAdminFormVisible === false && (
+							<Button onClick={loginAsOperator} color='primary'>
+								Login as Operator
+							</Button>
+						)}
+						{isAdminFormVisible === true && (
+							<Button
+								onClick={() => {
+									setIsAdminFormVisibility(false);
+								}}
+								color='secondary'
+							>
+								Back
+							</Button>
+						)}
+					</div>
 				</CardBody>
 			</Card>
 		</div>
