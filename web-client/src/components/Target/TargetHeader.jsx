fd9ca63887675240244ab1dc2c8c2deccd81adc1
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-06-29 19:33:39 +0530

    Target screen ui change

--- web-client/src/components/Target/TargetHeader.jsx
+++ web-client/src/components/Target/TargetHeader.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Text } from 'shared-components';
+import { Text, ButtonIcon } from 'shared-components';
 
 const StyledTargetHeader = styled.header`
-	padding: 0 48px 20px 88px;
+	padding: 0 48px 16px 88px;
 
 	h6.text-default {
 		line-height: 22px;
+	}
+
+	.text-truncate-multi-line {
+		height: 50px;
+		overflow: hidden;
 	}
 `;
 
@@ -24,7 +29,26 @@ const TargetHeader = (props) => {
 					Template Name
 				</Text>
 			)}
-			{isLoginTypeAdmin === true && ''}
+			{isLoginTypeAdmin === true && (
+				<div className='d-flex'>
+					<ButtonIcon name='pencil' size={28} className='px-0' />
+					<Text
+						size={14}
+						className='flex-25 text-default text-truncate-multi-line font-weight-light mb-0 pl-3 pr-2 py-1'
+					>
+						Template Name
+					</Text>
+					<Text
+						size={14}
+						className='flex-100 text-default text-truncate-multi-line font-weight-light mb-0 px-2 py-1'
+					>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere,
+						quo autem? Veniam eligendi sit cum! Ratione pariatur dolorem impedit
+						dolorum perferendis temporibus, quam veritatis ducimus nesciunt,
+						nihil blanditiis eius nobis.
+					</Text>
+				</div>
+			)}
 		</StyledTargetHeader>
 	);
 };
