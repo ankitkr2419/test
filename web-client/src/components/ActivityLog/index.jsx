3087a8d7b8d20ea54049f5a794b92950a6452809
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-07-01 21:26:52 +0530

    Activity log table ui

--- web-client/src/components/ActivityLog/index.jsx
+++ web-client/src/components/ActivityLog/index.jsx
@@ -1,65 +1,60 @@
 import React from 'react';
 import { Table } from 'core-components';
+import { ButtonIcon } from 'shared-components';
 import './activity.scss';
+import ActivityData from './ActivityData.json';
+
+const headers = ActivityData.headers;
+const experiments = ActivityData.experiments;
 
 const ActivityComponent = (props) => {
 	return (
 		<div className='activity-content h-100 py-0'>
 			<div className='table-responsive'>
-				<Table striped bordered className='table-log'>
+				<Table striped className='table-log'>
+					<colgroup>
+						<col width='9%' />
+						<col />
+						<col width='12%' />
+						<col width='10.5%' />
+						<col width='9.5%' />
+						<col width='8%' />
+						<col width='8%' />
+						<col width='12%' />
+						<col width='15%' />
+					</colgroup>
 					<thead>
 						<tr>
-							<th>Exp ID</th>
-							<th>Template</th>
-							<th>Date</th>
-							<th>Start time</th>
-							<th>End Time</th>
-							<th>No. Of Wells</th>
-							<th>Repeat Cycles</th>
-							<th>Result</th>
+							{headers.map((header, i) => (
+								<th key={i}>{header}</th>
+							))}
+							<th />
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>ID001</td>
-							<td>Template Name</td>
-							<td>23/05/2020</td>
-							<td>23:50 PM</td>
-							<td>01:21 AM</td>
-							<td>60</td>
-							<td>5</td>
-							<td>Successful</td>
-						</tr>
-						<tr>
-							<td>ID001</td>
-							<td>Template Name</td>
-							<td>23/05/2020</td>
-							<td>23:50 PM</td>
-							<td>01:21 AM</td>
-							<td>60</td>
-							<td>5</td>
-							<td>Successful</td>
-						</tr>
-						<tr>
-							<td>ID001</td>
-							<td>Template Name</td>
-							<td>23/05/2020</td>
-							<td>23:50 PM</td>
-							<td>01:21 AM</td>
-							<td>60</td>
-							<td>5</td>
-							<td>Successful</td>
-						</tr>
-						<tr>
-							<td>ID001</td>
-							<td>Template Name</td>
-							<td>23/05/2020</td>
-							<td>23:50 PM</td>
-							<td>01:21 AM</td>
-							<td>60</td>
-							<td>5</td>
-							<td>Successful</td>
-						</tr>
+						{experiments.map((experiment, i) => (
+							<tr
+								className={experiment.result === 'Inconclusive' ? 'active' : ''}
+								key={i}
+							>
+								<td>{experiment.id}</td>
+								<td>{experiment.template}</td>
+								<td>{experiment.date}</td>
+								<td>{experiment.start_time}</td>
+								<td>{experiment.end_time}</td>
+								<td>{experiment.no_of_wells}</td>
+								<td>{experiment.repeat_cycles}</td>
+								<td
+									className={experiment.result === 'Error' ? 'text-danger' : ''}
+								>
+									{experiment.result}
+								</td>
+								<td className='td-actions'>
+									<ButtonIcon size={28} name='expand' />
+									<ButtonIcon size={28} name='trash' />
+								</td>
+							</tr>
+						))}
 					</tbody>
 				</Table>
 			</div>
