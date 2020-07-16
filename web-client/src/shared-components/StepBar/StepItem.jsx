f40004e0ae683cf64cdc4dad4b0302a284ac40d6
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2020-07-16 19:49:26 +0530

    StepItem visited state

--- web-client/src/shared-components/StepBar/StepItem.jsx
+++ web-client/src/shared-components/StepBar/StepItem.jsx
@@ -4,28 +4,31 @@ import PropTypes from "prop-types";
 import bgStep from "../../assets/images/steps.svg";
 
 export const StepItem = styled(NavItem)`
-	width: 185px;
-	height: 40px;
-	background: transparent url(${bgStep}) no-repeat;
-	background-size: auto 84px;
-	background-position: -22px -19px;
-	margin: 0;
-	padding: 0 23px;
-	text-align: center;
-	opacity: ${(props) => (props.disabled ? "0.53" : "")};
-	pointer-events: ${(props) => (props.disabled ? "none" : "")};
+  width: 185px;
+  height: 40px;
+  background: transparent url(${bgStep}) no-repeat;
+  background-size: auto 84px;
+  background-position: -22px -19px;
+  margin: 0;
+  padding: 0 23px;
+  text-align: center;
+  opacity: ${(props) =>
+    props.disabled ? "0.40" : "" || props.visited ? "0.80" : ""};
+  pointer-events: ${(props) => (props.disabled ? "none" : "")};
 
-	+ .nav-item {
-		margin: 0 0 0 -16px;
-	}
+  + .nav-item {
+    margin: 0 0 0 -16px;
+  }
 `;
 
 StepItem.passProps = false;
 
 StepItem.propTypes = {
-	disabled: PropTypes.bool,
+  disabled: PropTypes.bool,
+  visited: PropTypes.bool,
 };
 
 StepItem.defaultProps = {
-	disabled: false
+  disabled: false,
+  visited: false,
 };
