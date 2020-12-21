5f63730215e6e4d5d7ade2abc41826ad6d11954c
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-21 16:46:39 +0530

    cartridge_num renamed to cartridge_id and subtype changed to just type in piercing table

--- migrations/1608223534_create_piercing_table.down.sql
+++ migrations/1608223534_create_piercing_table.down.sql
@@ -1,5 +1,3 @@
 DROP TABLE IF EXISTS piercing;
 
 DROP TYPE IF EXISTS discard_type;
-
-DROP TYPE IF EXISTS piercing_subtype;
--- migrations/1608223534_create_piercing_table.up.sql
+++ migrations/1608223534_create_piercing_table.up.sql
@@ -1,14 +1,12 @@
-CREATE TYPE piercing_subtype AS ENUM('extraction', 'pcr');
-
 CREATE TYPE discard_type AS ENUM ('at_pickup_passing', 'at_discard_box');
 
 CREATE TABLE IF NOT EXISTS piercing(
  id uuid,
- subtype piercing_subtype DEFAULT 'extraction',
- cartridge_num int,
+ cartridge_id int,
+ type cartridge_type,
  wells integer[] NOT NULL,
  discard discard_type DEFAULT 'at_pickup_passing',
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY (id) REFERENCES processes(id),
-FOREIGN KEY (cartridge_num) REFERENCES cartridges(number) );
+ FOREIGN KEY (id) REFERENCES processes(id),
+ FOREIGN KEY (cartridge_id, type) REFERENCES cartridges(id, type));
