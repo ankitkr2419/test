25ad7bfd297f83d5647b8688b83f95bea6490fd0
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-21 16:17:04 +0530

    recipes now has a labware_id to refer to

--- migrations/1608223267_create_recipes_table.up.sql
+++ migrations/1608223267_create_recipes_table.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS recipes(
  id uuid primary key default uuid_generate_v4(),
  name varchar(50) NOT NULL,
  description varchar(200) NOT NULL,
+ labware_id int NOT NULL,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
- );
+ updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ FOREIGN KEY (labware_id) references labwares(id));
