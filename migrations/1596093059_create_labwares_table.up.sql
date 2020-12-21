194d32206b1a2abed6d259821262beb1c1e55914
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-21 19:17:48 +0530

    labwares table will be a base table for tips_and_tubes and cartridges

--- migrations/1596093059_create_labwares_table.up.sql
+++ migrations/1596093059_create_labwares_table.up.sql
@@ -1,14 +1,4 @@
-CREATE TYPE labware_type AS ENUM('tip', 'tube');
-CREATE TYPE labware_subtype AS ENUM('extraction', 'pcr', 'piercing', 'sample' , 'shaker');
-
 CREATE TABLE IF NOT EXISTS labwares(
-id int NOT NULL,
-name varchar(50) NOT NULL, 
-type labware_type,
-subtype labware_subtype,
-volume decimal NOT NULL,
-height decimal NOT NULL,
-created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id, name),
-FOREIGN KEY (name) REFERENCES consumable_distances(name));
+ id int PRIMARY KEY,
+ name varchar(50) NOT NULL,
+ description varchar(250) NOT NULL);
