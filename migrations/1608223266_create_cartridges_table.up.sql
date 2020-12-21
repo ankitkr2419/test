2d8e6dae651af559856859e33fddfac89de88952
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-21 16:16:10 +0530

    cartridges migrations changed with some FK constarints added

--- migrations/1608223266_create_cartridges_table.up.sql
+++ migrations/1608223266_create_cartridges_table.up.sql
@@ -1,15 +1,14 @@
 CREATE TYPE cartridge_type AS ENUM('extraction', 'pcr');
 
 CREATE TABLE IF NOT EXISTS cartridges(
- number serial primary key,
+ id int NOT NULL,
  type cartridge_type DEFAULT 'extraction',
+ description varchar(250) NOT NULL,
  wells int DEFAULT 8 NOT NULL,
  distances decimal[] NOT NULL,
  heights decimal[] NOT NULL,
  volumes decimal[] NOT NULL,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT distances_array_len CHECK(wells = array_length(distances, 1) ),
-CONSTRAINT heights_array_len CHECK( wells = array_length(heights, 1) ),
-CONSTRAINT volumes_array_len CHECK( wells = array_length(volumes, 1) )
-);
+ FOREIGN KEY (id) REFERENCES labwares(id),
+ PRIMARY KEY (id, type));
