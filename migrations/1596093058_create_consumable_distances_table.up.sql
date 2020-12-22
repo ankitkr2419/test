e406ab1b1a7c0de7a18a8cb6049cfd74abbd9087
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-22 21:17:12 +0530

    deck_1_distance renamed to just distance and it is NOT NULL

--- migrations/1596093058_create_consumable_distances_table.up.sql
+++ migrations/1596093058_create_consumable_distances_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS consumable_distances(
  id int PRIMARY KEY,
  name varchar(50) UNIQUE NOT NULL,
  description varchar(250),
- deck_1_distance decimal NULL,
+ distance decimal NOT NULL,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP );
