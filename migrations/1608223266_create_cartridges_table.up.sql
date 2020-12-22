1fb2fdadcd72de038d4c2f0d190d7a7a31648fb5
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-22 20:43:01 +0530

    for cartridges table we will insert info as per each well separately

--- migrations/1608223266_create_cartridges_table.up.sql
+++ migrations/1608223266_create_cartridges_table.up.sql
@@ -3,12 +3,12 @@ CREATE TYPE cartridge_type AS ENUM('extraction', 'pcr');
 CREATE TABLE IF NOT EXISTS cartridges(
  labware_id int NOT NULL,
  type cartridge_type DEFAULT 'extraction',
- description varchar(250) NOT NULL,
- wells int DEFAULT 8 NOT NULL,
- distances decimal[] NOT NULL,
- heights decimal[] NOT NULL,
- volumes decimal[] NOT NULL,
+ description varchar(250),
+ well_num int NOT NULL,
+ distance decimal NOT NULL,
+ height decimal NOT NULL,
+ volume decimal NOT NULL,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- FOREIGN KEY (labware_id) REFERENCES labwares(id),
- PRIMARY KEY (labware_id, type));
+ FOREIGN KEY (labware_id) REFERENCES labwares(id) ON UPDATE CASCADE ON DELETE CASCADE,
+ PRIMARY KEY (labware_id, type, well_num));
