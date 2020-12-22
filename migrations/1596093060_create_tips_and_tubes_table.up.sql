f012293ec8278e1c147d6c7264e2974e96de5b57
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-22 20:40:45 +0530

    instead of refering to name from consumable distances table we refer to its id

--- migrations/1596093060_create_tips_and_tubes_table.up.sql
+++ migrations/1596093060_create_tips_and_tubes_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS tips_and_tubes(
 labware_id int NOT NULL,
+consumable_distance_id int NOT NULL,
 name varchar(50) NOT NULL,
 volume decimal NOT NULL,
 height decimal NOT NULL,
 created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (labware_id, name),
-FOREIGN KEY (name) REFERENCES consumable_distances(name),
-FOREIGN KEY (labware_id) REFERENCES labwares(id));
+FOREIGN KEY (consumable_distance_id) REFERENCES consumable_distances(id) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (labware_id) REFERENCES labwares(id) ON UPDATE CASCADE ON DELETE CASCADE);
