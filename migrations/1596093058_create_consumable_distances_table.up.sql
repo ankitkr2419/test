2b654643d5ff89063f2a7c6f650df3ed060bc36a
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-22 20:38:10 +0530

    id field added in consumable distances table

--- migrations/1596093058_create_consumable_distances_table.up.sql
+++ migrations/1596093058_create_consumable_distances_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS consumable_distances(
-name varchar(50) PRIMARY KEY,
-description varchar(250),
-deck_1_distance decimal NULL,
-created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP );
+ id int PRIMARY KEY,
+ name varchar(50) UNIQUE NOT NULL,
+ description varchar(250),
+ deck_1_distance decimal NULL,
+ created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP );
