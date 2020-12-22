a632eb6261795e7a894b030b6d66edd069971b96
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2020-12-22 20:58:55 +0530

    piercing has well_num instead of array of wells.

--- migrations/1608223534_create_piercing_table.up.sql
+++ migrations/1608223534_create_piercing_table.up.sql
@@ -4,9 +4,10 @@ CREATE TABLE IF NOT EXISTS piercing(
  id uuid,
  cartridge_id int,
  type cartridge_type,
- wells integer[] NOT NULL,
+ well_num integer NOT NULL,
  discard discard_type DEFAULT 'at_pickup_passing',
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (id, well_num),
  FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE,
  FOREIGN KEY (cartridge_id, type) REFERENCES cartridges(labware_id, type) ON UPDATE CASCADE ON DELETE CASCADE);
