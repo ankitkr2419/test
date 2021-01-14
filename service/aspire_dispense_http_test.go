29f9323fb1058f86feae39c470673e2d9192fc33
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-01-14 19:58:54 +0530

    Unnecessary code removed

--- service/aspire_dispense_http_test.go
+++ service/aspire_dispense_http_test.go
@@ -63,21 +63,7 @@ func (suite *AspireDispenseHandlerTestSuite) TestCreateAspireDispenseSuccess() {
 
 func (suite *AspireDispenseHandlerTestSuite) TestCreateAspireDispenseFailure() {
 	testUUID := uuid.New()
-	suite.dbMock.On("CreateAspireDispense", mock.Anything, mock.Anything).Return(db.AspireDispense{
-		ID:                 testUUID,
-		Category:           "well_to_well",
-		WellNoSource:       1,
-		AspireHeight:       2,
-		AspireMixingVolume: 3,
-		AspireNoOfCycles:   4,
-		AspireVolume:       5,
-		DispenseHeight:     6,
-		DispenseMixingVol:  7,
-		DispenseNoOfCycles: 8,
-		DispenseVol:        9,
-		DispenseBlow:       10,
-		WellToDestination:  11,
-	}, fmt.Errorf("Error creating aspire dispense"))
+	suite.dbMock.On("CreateAspireDispense", mock.Anything, mock.Anything).Return(db.AspireDispense{}, fmt.Errorf("Error creating aspire dispense"))
 
 	body := fmt.Sprintf(`{"id":"%s","category":"well_to_well","well_no_source":1,"aspire_height":2,"aspire_mixing_volume":3,"aspire_no_of_cycles":4,"aspire_volume":5,"dispense_height":6,"dispense_mixing_volume":7,"dispense_no_of_cycles":8,"dispense_vol":9,"dispense_blow":10,"well_to_destination":11,"created_at":"0001-01-01T00:00:00Z","updated_at":"0001-01-01T00:00:00Z"}`, testUUID)
 
@@ -130,25 +116,8 @@ func (suite *AspireDispenseHandlerTestSuite) TestListAspireDispenseSuccess() {
 }
 
 func (suite *AspireDispenseHandlerTestSuite) TestListAspireDispenseFailure() {
-	testUUID := uuid.New()
 	suite.dbMock.On("ListAspireDispense", mock.Anything, mock.Anything).Return(
-		[]db.AspireDispense{
-			db.AspireDispense{
-				ID:                 testUUID,
-				Category:           "well_to_well",
-				WellNoSource:       1,
-				AspireHeight:       2,
-				AspireMixingVolume: 3,
-				AspireNoOfCycles:   4,
-				AspireVolume:       5,
-				DispenseHeight:     6,
-				DispenseMixingVol:  7,
-				DispenseNoOfCycles: 8,
-				DispenseVol:        9,
-				DispenseBlow:       10,
-				WellToDestination:  11,
-			},
-		}, fmt.Errorf("Error fetching aspire dispense"))
+		[]db.AspireDispense{}, fmt.Errorf("Error fetching aspire dispense"))
 
 	recorder := makeHTTPCall(
 		http.MethodGet,
