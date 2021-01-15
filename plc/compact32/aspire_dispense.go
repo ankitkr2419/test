f87685fec68cd1d430220290ceec861c8b043084
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-01-15 12:06:27 +0530

    dispensing logic added with 1ul pulses= 25 added

--- plc/compact32/aspire_dispense.go
+++ plc/compact32/aspire_dispense.go
@@ -25,15 +25,15 @@ variables: category, cartridgeType string,
    		if its positive then direction is 1(towards sensor) else 0(oppose sensor)
   11. move deck to match the sourcePosition with help of difference calculated
   12. move syringe module down at fast till base
-  13. setup the syringe motor with aspire height
+  13. setup the syringe module motor with aspire height
   14. pickup and drop that asp_mix_vol for number of aspire_cycles
    these cycles should be fast
   15. pickup asp_vol slow
   16. move syringe module up slow till just above base
-  17. take air in 5ml
+  17. take air in 5ul
   18. Move slowly to destinationPosition by calculating the difference of Positions
   19. move syringe module down at fast till base
-  20. setup the syringe motor with dispense height
+  20. setup the syringe module motor with dispense height
   21. pickup and drop that dis_mix_vol for number of dis_cycles
   22. blow the air out
   23. move syringe module up
@@ -54,7 +54,7 @@ func (d *Compact32Deck) AspireDispense(category, cartridgeType string, labwareID
 	var sourceCartridge, destinationCartridge map[string]float64
 	var sourcePosition, destinationPosition, distToTravel, position, tipHeight float64
 	var ok bool
-	var direction, pulses, blow uint16
+	var direction, pulses uint16
 	var deckAndMotor DeckNumber
 
 	deckAndMotor.Deck = d.name
@@ -294,7 +294,10 @@ skipDeckToSourcePosition:
 	//
 
 	deckAndMotor.Number = K10_Syringe_LHRH
-	pulses = uint16(math.Round(float64(motors[deckAndMotor]["steps"]) * asp_mix_vol))
+	// for volume :-> 25 pulses = 1 microLitres
+	// NOTE: Store volumes in microLitres only
+	oneMicroLitrePulses := 25.0
+	pulses = uint16(math.Round(oneMicroLitrePulses * asp_mix_vol))
 
 	for cycleNumber := int64(1); cycleNumber <= aspire_cycles; cycleNumber++ {
 		// Aspire
@@ -313,6 +316,7 @@ skipDeckToSourcePosition:
 		time.Sleep(100 * time.Millisecond)
 	}
 
+	pulses = uint16(math.Round(oneMicroLitrePulses * asp_vol))
 	//
 	// ALGORITHM's 15th step is implemented below
 	//
@@ -328,7 +332,168 @@ skipDeckToSourcePosition:
 	//
 	deckAndMotor.Number = K9_Syringe_Module_LHRH
 
-	// TODO: Just blow 5 ml Pulses for now
+	if position, ok = consDistance["pickup_"+cartridgeType+"_tip_up"]; !ok {
+		err = fmt.Errorf("pickup_" + cartridgeType + "_tip_up doesn't exist for consuamble distances")
+		fmt.Println("Error: ", err)
+		return "", err
+	}
+	// Don't forget to add tipHeight for every tip we have currently attached
+	distToTravel = positions[deckAndMotor] + tipHeight - position
+	pulses = uint16(math.Round(float64(motors[deckAndMotor]["steps"]) * distToTravel))
 
-	return
+	response, err = d.SetupMotor(motors[deckAndMotor]["slow"], pulses, motors[deckAndMotor]["ramp"], UP, deckAndMotor.Number)
+
+	deckAndMotor.Number = K10_Syringe_LHRH
+
+	//
+	// ALGORITHM's 17th step is implemented below
+	//
+	// Just aspire 5 ul Pulses for now
+	// TODO: Remove this hardcoded volume
+	pulses = uint16(math.Round(oneMicroLitrePulses * 5))
+	response, err = d.SetupMotor(motors[deckAndMotor]["slow"], pulses, motors[deckAndMotor]["ramp"], ASPIRE, deckAndMotor.Number)
+	if err != nil {
+		return
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	//18. Move slowly to destinationPosition by calculating the difference of Positions
+	//19. move syringe module down at slow till base
+	//20. setup the syringe module motor with dispense height
+	//21. pickup and drop that dis_mix_vol for number of dis_cycles
+	//22. blow the air out
+	//23. move syringe module up
+	//
+	// ALGORITHM's 18th step is implemented below
+	//
+
+	deckAndMotor.Number = K5_Deck
+
+	distToTravel = positions[deckAndMotor] - destinationPosition
+
+	switch {
+	// distToTravel > 0 means go towards the Sensor or FWD
+	case distToTravel > 0:
+		direction = 1
+	case distToTravel < 0:
+		distToTravel *= -1
+		direction = 0
+	default:
+		// Skip the setUpMotor Step
+		goto skipDeckToDestinationPosition
+	}
+
+	pulses = uint16(math.Round(float64(motors[deckAndMotor]["steps"]) * distToTravel))
+
+	response, err = d.SetupMotor(motors[deckAndMotor]["fast"], pulses, motors[deckAndMotor]["ramp"], direction, deckAndMotor.Number)
+	if err != nil {
+		fmt.Println(err)
+		return "", fmt.Errorf("There was issue moving Deck to Dispense Destination. Error: %v", err)
+	}
+	time.Sleep(100 * time.Millisecond)
+
+skipDeckToDestinationPosition:
+
+	//
+	// ALGORITHM's 19th step is implemented below
+	//
+
+	// We know the concrete direction here onwards
+	deckAndMotor.Number = K9_Syringe_Module_LHRH
+	// TODO Add deck_base in consDistance
+	if position = consDistance["deck_base"]; ok {
+		distToTravel = position - (positions[deckAndMotor] + tipHeight)
+	} else {
+		err = fmt.Errorf("deck_base doesn't exist for consuamble distances")
+		fmt.Println("Error: ", err)
+		return "", err
+	}
+
+	pulses = uint16(math.Round(float64(motors[deckAndMotor]["steps"]) * distToTravel))
+
+	response, err = d.SetupMotor(motors[deckAndMotor]["slow"], pulses, motors[deckAndMotor]["ramp"], DOWN, deckAndMotor.Number)
+	if err != nil {
+		fmt.Println(err)
+		return "", fmt.Errorf("There was issue moving Syringe Module with tip to deck base. Error: %v", err)
+	}
+	time.Sleep(100 * time.Millisecond)
+
+	//
+	// ALGORITHM's 20th step is implemented below
+	//
+
+	pulses = uint16(math.Round(float64(motors[deckAndMotor]["steps"]) * dis_height))
+
+	response, err = d.SetupMotor(motors[deckAndMotor]["slow"], pulses, motors[deckAndMotor]["ramp"], DOWN, deckAndMotor.Number)
+	if err != nil {
+		fmt.Println(err)
+		return "", fmt.Errorf("There was issue moving Syringe Module with tip. Error: %v", err)
+	}
+	time.Sleep(100 * time.Millisecond)
+
+	//
+	// ALGORITHM's 21th step is implemented below
+	//
+
+	deckAndMotor.Number = K10_Syringe_LHRH
+	pulses = uint16(math.Round(oneMicroLitrePulses * dis_mix_vol))
+
+	for cycleNumber := int64(1); cycleNumber <= dispense_cycles; cycleNumber++ {
+		// Dispense
+		response, err = d.SetupMotor(motors[deckAndMotor]["fast"], pulses, motors[deckAndMotor]["ramp"], DISPENSE, deckAndMotor.Number)
+		if err != nil {
+			return
+		}
+
+		time.Sleep(100 * time.Millisecond)
+
+		response, err = d.SetupMotor(motors[deckAndMotor]["fast"], pulses, motors[deckAndMotor]["ramp"], ASPIRE, deckAndMotor.Number)
+		if err != nil {
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	//
+	// ALGORITHM's 22nd step is implemented below
+	//
+	pulses = uint16(math.Round(oneMicroLitrePulses * dis_vol))
+	response, err = d.SetupMotor(motors[deckAndMotor]["fast"], pulses, motors[deckAndMotor]["ramp"], DISPENSE, deckAndMotor.Number)
+	if err != nil {
+		return
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	//
+	// ALGORITHM's 23th step is implemented below
+	//
+	deckAndMotor.Number = K9_Syringe_Module_LHRH
+
+	if position, ok = consDistance["pickup_"+cartridgeType+"_tip_up"]; !ok {
+		err = fmt.Errorf("pickup_" + cartridgeType + "_tip_up doesn't exist for consuamble distances")
+		fmt.Println("Error: ", err)
+		return "", err
+	}
+	// Don't forget to add tipHeight for every tip we have currently attached
+	distToTravel = positions[deckAndMotor] + tipHeight - position
+	pulses = uint16(math.Round(float64(motors[deckAndMotor]["steps"]) * distToTravel))
+
+	response, err = d.SetupMotor(motors[deckAndMotor]["slow"], pulses, motors[deckAndMotor]["ramp"], UP, deckAndMotor.Number)
+	if err != nil {
+		return
+	}
+
+	deckAndMotor.Number = K10_Syringe_LHRH
+	// blowing air after we are UP
+	pulses = uint16(math.Round(oneMicroLitrePulses * dis_blow))
+	response, err = d.SetupMotor(motors[deckAndMotor]["slow"], pulses, motors[deckAndMotor]["ramp"], DISPENSE, deckAndMotor.Number)
+	if err != nil {
+		return
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	return "ASPIRE and DISPENSE was successful", nil
 }
