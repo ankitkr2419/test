ff00945ec22eed8d7369659b49e1fc0f2cbc4c65
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:06:47 +0530

    heating table updated

--- migrations/1608223695_create_heating_table.up.sql
+++ migrations/1608223695_create_heating_table.up.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS heating(
- id uuid,
- temperature decimal,
+ id uuid primary key,
+ temperature int,
  follow_temp boolean DEFAULT FALSE,
- duration decimal,
+ duration int,
+ shaker_no int,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE);
