0f258fbbfa796e8fb417701882ae51cb483d2928
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:50:51 +0530

    aspire dispense table made flexible for source, dest positions

--- migrations/1610529998_create_aspire_dispense.up.sql
+++ migrations/1610529998_create_aspire_dispense.up.sql
@@ -3,7 +3,7 @@ CREATE TYPE aspire_dispense_type AS ENUM ('well_to_shaker', 'shaker_to_well', 'w
 CREATE TABLE IF NOT EXISTS aspire_dispense(
  id uuid PRIMARY KEY,
  category aspire_dispense_type,
- well_no_source int,
+ source_pos int,
  aspire_height float,
  aspire_mixing_volume float,
  aspire_no_of_cycles int,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS aspire_dispense(
  dispense_no_of_cycles int,
  dispense_vol float,
  dispense_blow float,
- well_no_destination int,
+ destination_pos int,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE);
