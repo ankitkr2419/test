ddff7a3be9b0337742de74bcc2196c2b14573b58
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:07:01 +0530

    shaking table created

---
+++ migrations/1608223885_create_shaking_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS shaking(
+ id uuid primary key,
+ set_temperature boolean DEFAULT FALSE,
+ follow_temp boolean DEFAULT FALSE,
+ temperature int,
+ rpm_1 int,
+ rpm_2 int
+ time_1 int,
+ time_2 int,
+ created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE);
