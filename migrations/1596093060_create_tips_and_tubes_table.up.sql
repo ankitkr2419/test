91cdd29d2a501d33d4538310f7858a3260668e3c
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:05:29 +0530

    tips_tubes table changed

--- migrations/1596093060_create_tips_and_tubes_table.up.sql
+++ migrations/1596093060_create_tips_and_tubes_table.up.sql
@@ -1,11 +1,12 @@
+CREATE TYPE tip_tube_type AS ENUM('tip', 'tube');
+
 CREATE TABLE IF NOT EXISTS tips_and_tubes(
-labware_id int NOT NULL,
-consumable_distance_id int NOT NULL,
-name varchar(50) NOT NULL,
+id int PRIMARY KEY,
+name varchar(50) UNIQUE NOT NULL,
+type tip_tube_type,
+allowed_positions integer[],
 volume decimal NOT NULL,
 height decimal NOT NULL,
 created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (labware_id, name),
-FOREIGN KEY (consumable_distance_id) REFERENCES consumable_distances(id) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY (labware_id) REFERENCES labwares(id) ON UPDATE CASCADE ON DELETE CASCADE);
+updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
