ec59c5e18451e3b284899bb169d0d1e70c67100b
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:49:29 +0530

    cartridges table broken into cartridges and cartridges_wells

--- migrations/1608223266_create_cartridges_table.up.sql
+++ migrations/1608223266_create_cartridges_table.up.sql
@@ -1,14 +1,9 @@
 CREATE TYPE cartridge_type AS ENUM('cartridge_1', 'cartridge_2');
  
 CREATE TABLE IF NOT EXISTS cartridges(
- id int NOT NULL,
+ id int PRIMARY KEY,
  type cartridge_type DEFAULT 'cartridge_1',
  description varchar(250),
- well_num int NOT NULL,
- distance decimal NOT NULL,
- height decimal NOT NULL,
- volume decimal NOT NULL,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id, well_num)
+ updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
