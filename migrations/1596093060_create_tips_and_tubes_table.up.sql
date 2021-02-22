69608111897cb4046c5d7a8e0e8127d08efe1f5d
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:48:49 +0530

    integer changed to just int

--- migrations/1596093060_create_tips_and_tubes_table.up.sql
+++ migrations/1596093060_create_tips_and_tubes_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS tips_and_tubes(
 id int PRIMARY KEY,
 name varchar(50) UNIQUE NOT NULL,
 type tip_tube_type,
-allowed_positions integer[],
+allowed_positions int[],
 volume decimal NOT NULL,
 height decimal NOT NULL,
 created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
