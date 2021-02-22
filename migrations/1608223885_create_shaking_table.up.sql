211ee9be343c8c7a0bf68da9b25c5eee1c9358d3
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-22 21:50:06 +0530

    comma and capitalization added

--- migrations/1608223885_create_shaking_table.up.sql
+++ migrations/1608223885_create_shaking_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS shaking(
- id uuid primary key,
+ id uuid PRIMARY KEY,
  set_temperature boolean DEFAULT FALSE,
  follow_temp boolean DEFAULT FALSE,
  temperature int,
  rpm_1 int,
- rpm_2 int
+ rpm_2 int,
  time_1 int,
  time_2 int,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
