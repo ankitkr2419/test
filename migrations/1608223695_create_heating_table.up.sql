63380abc16b2fcfc82443a87c20d9a95fd6224f2
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-02-23 17:32:06 +0530

    removed shaker number from db and hardcoded its value

--- migrations/1608223695_create_heating_table.up.sql
+++ migrations/1608223695_create_heating_table.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS heating(
  temperature int,
  follow_temp boolean DEFAULT FALSE,
  duration int,
- shaker_no int,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE);
--- service/heating_http.go
+++ service/heating_http.go
@@ -11,6 +11,8 @@ import (
 func plcHeatingHandler(deps Dependencies) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		var heatingID uuid.UUID
+		//here we are hardcoding the shaker no in future this is to be fetched dynamically.
+		shakerNo := 3
 		vars := mux.Vars(req)
 		heatingID, err := parseUUID(vars["id"])
 		if err != nil {
@@ -24,7 +26,7 @@ func plcHeatingHandler(deps Dependencies) http.HandlerFunc {
 		fmt.Printf("heat object %v", heat)
 		switch deck {
 		case "A", "B":
-			_, err := deps.PlcDeck[deck].Heat(uint16(heat.Temperature), uint16(heat.ShakerNo), heat.FollowTemp, heat.Duration)
+			_, err := deps.PlcDeck[deck].Heat(uint16(heat.Temperature), uint16(shakerNo), heat.FollowTemp, heat.Duration)
 			if err != nil {
 				rw.WriteHeader(http.StatusInternalServerError)
 				return
