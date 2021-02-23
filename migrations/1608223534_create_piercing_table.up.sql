533e83f62c8f55d687d44e0bd1e3995344e6ad6a
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-23 13:32:02 +0530

    piercing table changes added

--- migrations/1608223534_create_piercing_table.up.sql
+++ migrations/1608223534_create_piercing_table.up.sql
@@ -1,10 +1,10 @@
 CREATE TYPE discard_type AS ENUM ('at_pickup_passing', 'at_discard_box');
 
 CREATE TABLE IF NOT EXISTS piercing(
- id uuid,
- cartridge_ids int[] NOT NULL,
+ id uuid PRIMARY KEY,
+ type cartridge_type DEFAULT 'cartridge_1',
+ cartridge_wells int[] NOT NULL,
  discard discard_type DEFAULT 'at_pickup_passing',
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
- PRIMARY KEY (id),
  FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE);
