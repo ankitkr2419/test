567315169a2a3708865700e24999cbedf1152b66
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-24 12:48:09 +0530

    AspireDispense Structure changed for DB operation

--- db/aspire_dispense.go
+++ db/aspire_dispense.go
@@ -14,22 +14,26 @@ const (
 	well_to_shaker Category = "well_to_shaker"
 	shaker_to_well Category = "shaker_to_well"
 	well_to_well   Category = "well_to_well"
+	well_to_deck   Category = "well_to_deck"
+	deck_to_well   Category = "deck_to_well"
+	deck_to_deck   Category = "deck_to_deck"
 )
 
 const (
 	getAspireDispenseQuery = `SELECT id,
 						category,
-						well_no_source,
+						source_position,
 						aspire_height,
 						aspire_mixing_volume,
 						aspire_no_of_cycles,
 						aspire_volume,
+						aspire_air_volume,
 						dispense_height,
 						dispense_mixing_volume,
 						dispense_no_of_cycles,
-						dispense_vol,
-						dispense_blow,
-						well_to_destination,
+						dispense_volume,
+						dispense_blow_volume,
+						destination_position,
 						created_at,
 						updated_at
 						FROM aspire_dispense
@@ -41,51 +45,54 @@ const (
 	createAspireDispenseQuery = `INSERT INTO aspire_dispense (
 						id,
 						category,
-						well_no_source,
+						source_position,
 						aspire_height,
 						aspire_mixing_volume,
 						aspire_no_of_cycles,
 						aspire_volume,
+						aspire_air_volume,
 						dispense_height,
 						dispense_mixing_volume,
 						dispense_no_of_cycles,
-						dispense_vol,
-						dispense_blow,
-						well_to_destination)
-						VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING id`
+						dispense_volume,
+						dispense_blow_volume,
+						destination_position)
+						VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING id`
 	updateAspireDispenseQuery = `UPDATE aspire_dispense SET (
 						category,
-						well_no_source,
+						source_position,
 						aspire_height,
 						aspire_mixing_volume,
 						aspire_no_of_cycles,
 						aspire_volume,
+						aspire_air_volume,
 						dispense_height,
 						dispense_mixing_volume,
 						dispense_no_of_cycles,
-						dispense_vol,
-						dispense_blow,
-						well_to_destination,
+						dispense_volume,
+						dispense_blow_volume,
+						destination_position,
 						updated_at) = 
-						($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) WHERE id = $14`
+						($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) WHERE id = $15`
 )
 
 type AspireDispense struct {
-	ID                 uuid.UUID `db:"id" json:"id"`
-	Category           Category  `db:"category" json:"category"`
-	WellNoSource       int64     `db:"well_no_source" json:"well_no_source"`
-	AspireHeight       float64   `db:"aspire_height" json:"aspire_height"`
-	AspireMixingVolume float64   `db:"aspire_mixing_volume" json:"aspire_mixing_volume"`
-	AspireNoOfCycles   int64     `db:"aspire_no_of_cycles" json:"aspire_no_of_cycles"`
-	AspireVolume       float64   `db:"aspire_volume" json:"aspire_volume"`
-	DispenseHeight     float64   `db:"dispense_height" json:"dispense_height"`
-	DispenseMixingVol  float64   `db:"dispense_mixing_volume" json:"dispense_mixing_volume"`
-	DispenseNoOfCycles int64     `db:"dispense_no_of_cycles" json:"dispense_no_of_cycles"`
-	DispenseVol        float64   `db:"dispense_vol" json:"dispense_vol"`
-	DispenseBlow       float64   `db:"dispense_blow" json:"dispense_blow"`
-	WellToDestination  int64     `db:"well_to_destination" json:"well_to_destination"`
-	CreatedAt          time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt          time.Time `db:"updated_at" json:"updated_at"`
+	ID                   uuid.UUID `db:"id" json:"id"`
+	Category             Category  `db:"category" json:"category"`
+	SourcePosition       int64     `db:"source_position" json:"source_position"`
+	AspireHeight         float64   `db:"aspire_height" json:"aspire_height"`
+	AspireMixingVolume   float64   `db:"aspire_mixing_volume" json:"aspire_mixing_volume"`
+	AspireNoOfCycles     int64     `db:"aspire_no_of_cycles" json:"aspire_no_of_cycles"`
+	AspireVolume         float64   `db:"aspire_volume" json:"aspire_volume"`
+	AspireAirVolume      float64   `db:"aspire_air_volume" json:"aspire_air_volume"`
+	DispenseHeight       float64   `db:"dispense_height" json:"dispense_height"`
+	DispenseMixingVolume float64   `db:"dispense_mixing_volume" json:"dispense_mixing_volume"`
+	DispenseNoOfCycles   int64     `db:"dispense_no_of_cycles" json:"dispense_no_of_cycles"`
+	DispenseVolume       float64   `db:"dispense_volume" json:"dispense_volume"`
+	DispenseBlowVolume   float64   `db:"dispense_blow_volume" json:"dispense_blow_volume"`
+	DestinationPosition  int64     `db:"destination_position" json:"destination_position"`
+	CreatedAt            time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt            time.Time `db:"updated_at" json:"updated_at"`
 }
 
 func (s *pgStore) ShowAspireDispense(ctx context.Context, id uuid.UUID) (dbAspireDispense AspireDispense, err error) {
@@ -112,17 +119,18 @@ func (s *pgStore) CreateAspireDispense(ctx context.Context, ad AspireDispense) (
 		createAspireDispenseQuery,
 		ad.ID,
 		ad.Category,
-		ad.WellNoSource,
+		ad.SourcePosition,
 		ad.AspireHeight,
 		ad.AspireMixingVolume,
 		ad.AspireNoOfCycles,
 		ad.AspireVolume,
+		ad.AspireAirVolume,
 		ad.DispenseHeight,
-		ad.DispenseMixingVol,
+		ad.DispenseMixingVolume,
 		ad.DispenseNoOfCycles,
-		ad.DispenseVol,
-		ad.DispenseBlow,
-		ad.WellToDestination,
+		ad.DispenseVolume,
+		ad.DispenseBlowVolume,
+		ad.DestinationPosition,
 	).Scan(&lastInsertID)
 
 	if err != nil {
@@ -151,17 +159,18 @@ func (s *pgStore) UpdateAspireDispense(ctx context.Context, ad AspireDispense) (
 	_, err = s.db.Exec(
 		updateAspireDispenseQuery,
 		ad.Category,
-		ad.WellNoSource,
+		ad.SourcePosition,
 		ad.AspireHeight,
 		ad.AspireMixingVolume,
 		ad.AspireNoOfCycles,
 		ad.AspireVolume,
+		ad.AspireAirVolume,
 		ad.DispenseHeight,
-		ad.DispenseMixingVol,
+		ad.DispenseMixingVolume,
 		ad.DispenseNoOfCycles,
-		ad.DispenseVol,
-		ad.DispenseBlow,
-		ad.WellToDestination,
+		ad.DispenseVolume,
+		ad.DispenseBlowVolume,
+		ad.DestinationPosition,
 		time.Now(),
 		ad.ID,
 	)
