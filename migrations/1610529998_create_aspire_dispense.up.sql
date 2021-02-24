3344d8ae639671f4cfbfd8d78507450c19120cd9
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-02-24 12:41:06 +0530

    complete names used in case of aspire dispence field names

--- migrations/1610529998_create_aspire_dispense.up.sql
+++ migrations/1610529998_create_aspire_dispense.up.sql
@@ -3,7 +3,7 @@ CREATE TYPE aspire_dispense_type AS ENUM ('well_to_shaker', 'shaker_to_well', 'w
 CREATE TABLE IF NOT EXISTS aspire_dispense(
  id uuid PRIMARY KEY,
  category aspire_dispense_type,
- source_pos int,
+ source_position int,
  aspire_height float,
  aspire_mixing_volume float,
  aspire_no_of_cycles int,
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS aspire_dispense(
  dispense_height float,
  dispense_mixing_volume float,
  dispense_no_of_cycles int,
- dispense_vol float,
- dispense_blow float,
- destination_pos int,
+ dispense_volume float,
+ dispense_blow_volume float,
+ destination_position int,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  FOREIGN KEY (id) REFERENCES processes(id) ON UPDATE CASCADE ON DELETE CASCADE);
