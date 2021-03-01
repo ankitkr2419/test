142a45aa4a98ba18a3ce99a0c1022f934d5fc115
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-03-01 13:30:32 +0530

    singleDeckOperation func has no switch case for deck checking as its already handled by the handlers

--- service/homing_http.go
+++ service/homing_http.go
@@ -35,33 +35,28 @@ func homingHandler(deps Dependencies) http.HandlerFunc {
 }
 
 func singleDeckOperation(deps Dependencies, deck, operation string) (response string, err error) {
-	switch deck {
-	case "A", "B":
 
-		// Compact32Deck is the type of deps.PlcDeck[deck]
-		result := reflect.ValueOf(deps.PlcDeck[deck]).MethodByName(operation).Call([]reflect.Value{})
-		// TODO : variadic parameters ought to be handled as well
-		//  this will make Call to any method generic
-		// TODO : Handle Panics with recover()
+	// Compact32Deck is the type of deps.PlcDeck[deck]
+	result := reflect.ValueOf(deps.PlcDeck[deck]).MethodByName(operation).Call([]reflect.Value{})
+	// TODO : variadic parameters ought to be handled as well
+	//  this will make Call to any method generic
+	// TODO : Handle Panics with recover()
 
-		if len(result) != 2 {
-			fmt.Println("result is different in this reflect Call !", result)
-			return "", fmt.Errorf("unexpected length result")
-		}
-
-		fmt.Println("Correct Result: ", result)
-		response = result[0].String()
-		if len(response) > 0 {
-			return
-		}
-		errRes := result[1].Interface()
-		if errRes != nil {
-			fmt.Println(errRes)
-			return "", fmt.Errorf("%v", errRes)
-		}
-
-	default:
-		err = fmt.Errorf("Check your deck name")
+	if len(result) != 2 {
+		fmt.Println("result is different in this reflect Call !", result)
+		return "", fmt.Errorf("unexpected length result")
 	}
+
+	fmt.Println("Correct Result: ", result)
+	response = result[0].String()
+	if len(response) > 0 {
+		return
+	}
+	errRes := result[1].Interface()
+	if errRes != nil {
+		fmt.Println(errRes)
+		return "", fmt.Errorf("%v", errRes)
+	}
+
 	return
 }
