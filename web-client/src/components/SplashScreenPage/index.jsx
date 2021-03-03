39dc07b7a0b65e29b3def545fa816ea4e1692159
Dhanraj Lambhate <dhanraj.lambhate.jc@joshsoftware.com>
2021-03-03 13:24:09 +0530

    Add Styled Component CSS

--- web-client/src/components/SplashScreenPage/index.jsx
+++ web-client/src/components/SplashScreenPage/index.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
+import styled from 'styled-components'
 import { ImageIcon } from 'shared-components';
-import './splashscreen.scss';
 import CirclelogoIcon from 'assets/images/mylab-logo-with-circle.svg';
 
+const SplashScreen = styled.div`
+    background: url('/images/logo-bg.svg') left -4.875rem top -5.5rem no-repeat,
+    url('/images/honey-bees-bg.svg') right -1.75rem bottom -1.5rem no-repeat;
+`;
+const CircleImg = styled.div`
+    margin-right: 14.313rem;
+    margin-left: auto;
+`;
 
 const SplashScreenComponent = (props) => {
 	return (
-		<div className='splash-screen-content h-100 py-0 bg-white d-flex justify-content-center align-items-center'>
-            <ImageIcon 
-            src={CirclelogoIcon} 
-            alt="My Lab" 
-            className='mylab-logo-circle' />
-        </div>
+		<SplashScreen className='splash-screen-content h-100 py-0 bg-white d-flex justify-content-center align-items-center'>
+            <CircleImg>
+                <ImageIcon 
+                src={CirclelogoIcon} 
+                alt="My Lab" 
+                />
+            </CircleImg>
+        </SplashScreen>
 	);
 };
 
--- web-client/src/shared-components/SearchBox/index.jsx
+++ web-client/src/shared-components/SearchBox/index.jsx
@@ -1,24 +1,61 @@
 import React from 'react';
 
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import './searchBox.scss';
 
-const StyledSearchBox = styled(Link)`
-  display: flex;
-  width: ${props => (props.size === 'sm' ? '58px' : '150px')};
-  height: 48px;
-  align-items: center;
-  justify-content: center;
-  margin: ${props => (props.size === 'sm' ? '0 12px' : '')};
+const StyledSearchBox = styled.div`
+input {
+    outline: none;
+
+    &:-moz-placeholder {
+        color: rgba(102, 102, 102, 0.26);
+    }
+
+    &::-webkit-input-placeholder {
+        color: rgba(102, 102, 102, 0.26);
+    }
+}
+
+input[type=search] {
+    //-webkit-appearance: textfield;
+    //-webkit-box-sizing: content-box;
+    font-family: inherit;
+    font-size: 100%;
+    background: #fafafa url('images/search-icon.png') no-repeat center right 12px;
+    border: 1px solid #717171;
+    padding: 0.5rem 0.563rem 0.5rem 1.938rem;
+    width: 0px;
+    color: transparent;
+    cursor: pointer;
+    border-radius:10rem;
+    -webkit-transition: all .5s;
+    -moz-transition: all .5s;
+    transition: all .5s;
+    &:hover{
+        background-color: #fff;
+    }
+
+    &:focus {
+        width: 37.25rem;
+        padding-right: 2.5rem;
+        color: #000;
+        background-color: #fff;
+        cursor: auto;
+    }
+}
+
+input::-webkit-search-decoration,
+input::-webkit-search-cancel-button {
+    display: none;
+}
 `;
 
 const SearchBox = (props) => {
 	return (
-		<div className="d-flex justify-content-start align-items-center search-box-container m-3">
+		<StyledSearchBox className="d-flex justify-content-start align-items-center m-3">
 			<input type="search" placeholder="Search"/>
-		</div>
+		</StyledSearchBox>
 	);
 };
 
