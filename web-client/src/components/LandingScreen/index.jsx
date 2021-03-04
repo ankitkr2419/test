cc8774299b0f37a33772188f329938e57f1becc8
Dhanraj Lambhate <dhanraj.lambhate.jc@joshsoftware.com>
2021-03-04 20:03:14 +0530

    work on landing page

--- web-client/src/components/LandingScreen/index.jsx
+++ web-client/src/components/LandingScreen/index.jsx
@@ -1,18 +1,57 @@
 import React from 'react';
 
-import SearchBox from 'shared-components/SearchBox';
+import { Card, CardBody } from 'core-components';
+import {
+	ImageIcon,
+	Center,
+	Icon
+} from 'shared-components';
+
+// import SearchBox from 'shared-components/SearchBox';
+// import ButtonBar from 'shared-components/ButtonBar';
 import DeckCard from 'shared-components/DeckCard';
-import ButtonBar from 'shared-components/ButtonBar';
+import imgNoTemplate from 'assets/images/video-thumbnail.png';
+import styled from 'styled-components';
+
+const VideoPlayButton = styled.button`
+	color:#7C7976;
+	background-color:transparent;
+	border:0;
+	outline:none;
+	position:absolute;
+	top:50%;
+	left:50%;
+	transform:translate(-50%,-50%);
+`;
 
 const LandingScreenComponent = (props) => {
 	return (
-		<div className='landing-content h-100 py-0'>
-            <SearchBox/>
+		<div className="ml-content">
+			<div className='landing-content'>
+			<Card className='card-video'>
+				<CardBody className='d-flex flex-column p-0'>
+					<Center className="video-thumbnail-wrapper">
+					<ImageIcon
+						src={imgNoTemplate}
+						alt="No templates available"
+						className="img-video-thumbnail"
+					/>
+					<VideoPlayButton>
+						<Icon name="play" size={124}/>
+					</VideoPlayButton>
+					</Center>
+				</CardBody>
+			</Card>
+			
+				{/* <SearchBox/> */}
+				
+				{/* <ButtonBar/> */}
+			</div>
+
 			<div className="d-flex justify-content-center align-items-center">
 				<DeckCard/>
 				<DeckCard/>
 			</div>
-			<ButtonBar/>
 		</div>
 	);
 };
