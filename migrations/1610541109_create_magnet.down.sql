6b46d3dbcc2f0a91ffd606238837488c670f9e45
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-03-09 12:40:27 +0530

    deleted not required migration files and added drop type in down sql

--- migrations/1610541109_create_magnet.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS magnet;
- 
-DROP TYPE IF EXISTS magnet_movement_type;
--- migrations/1614592944_create_attach_detach.down.sql
+++ migrations/1614592944_create_attach_detach.down.sql
@@ -1,1 +1,6 @@
+--FIRST DROP TABLE
 DROP TABLE IF EXISTS attach_detach
+
+-- THEN DROP TYPE
+DROP TYPE IF EXISTS magnet_operation_type;
+DROP TYPE IF EXISTS magnet_operation_subtype;
