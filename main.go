db3dbe3578ec11fef19129b92b8bb7295259b99f
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-03-22 13:13:21 +0530

    simulator added for UI testing

--- main.go
+++ main.go
@@ -128,9 +128,12 @@ func startApp(plcName string, test bool) (err error) {
 	if plcName == "compact32" {
 		driver = compact32.NewCompact32Driver(exit, test)
 		driverDeckA, handler = compact32.NewCompact32DeckDriverA(exit, test)
-		driverDeckB = compact32.NewCompact32DeckDriverB(exit, test, handler)
+		driverDeckB = compact32.NewCompact32DeckDriverB(exit, test, handler)	
 	} else {
 		driver = simulator.NewSimulator(exit)
+		driverDeckA = simulator.NewUISimulator(exit,"A")
+		driverDeckB = simulator.NewUISimulator(exit,"B")
+
 	}
 
 	store, err = db.Init()
