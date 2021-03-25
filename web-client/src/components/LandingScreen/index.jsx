d86ba56d66156494511f62b9031eb347f93540d2
Aryan Jain <aryanjn807@gmail.com>
2021-03-25 15:36:02 +0530

    Updated till run button

--- web-client/src/components/LandingScreen/index.jsx
+++ web-client/src/components/LandingScreen/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import ReusableModal from "shared-components/ReusableModal";
 // import ConfirmationModal from "components/modals/ConfirmationModal";
 
 // import SearchBox from 'shared-components/SearchBox';
@@ -9,38 +8,36 @@ import AppFooter from "components/AppFooter";
 import { MODAL_MESSAGE, MODAL_BTN } from "appConstants";
 import { homingActionInitiated } from "action-creators/homingActionCreators";
 // import TimeModal from "components/modals/TimeModal";
-import { VideoCard } from "shared-components/index";
+import { VideoCard, MlModal } from "shared-components";
 
 const LandingScreenComponent = (props) => {
   const [homingStatus, setHomingStatus] = useState(true);
   const dispatch = useDispatch();
 
-  const homingConfirmation = (isConfirmed) => {
-    setHomingStatus(isConfirmed);
-    if (isConfirmed) {
-      dispatch(homingActionInitiated());
-    }
+  const homingConfirmation = () => {
+    dispatch(homingActionInitiated());
+    setHomingStatus(!homingStatus);
   };
+
   return (
     <div className="ml-content">
       <div className="landing-content">
-
         <VideoCard />
 
-        <ReusableModal
+        <MlModal
           isOpen={homingStatus}
-          toggleModal={homingConfirmation}
           textBody={MODAL_MESSAGE.homingConfirmation}
-          clickHandler={homingConfirmation}
-          primaryBtn={MODAL_BTN.okay}
+          handleSuccessBtn={homingConfirmation}
+          successBtn={MODAL_BTN.okay}
+          showCrossBtn={false}
         />
 
       </div>
-      <AppFooter 
-          loginBtn={true}
-          // deckName="Deck A" 
-          // showProcess={true} 
-          // showCleanUp={true}
+      <AppFooter
+        loginBtn={true}
+        // deckName="Deck A"
+        // showProcess={true}
+        // showCleanUp={true}
       />
     </div>
   );
--- web-client/src/shared-components/index.jsx
+++ web-client/src/shared-components/index.jsx
@@ -1,45 +1,47 @@
-import ButtonGroup from './ButtonGroup';
-import ButtonIcon from './ButtonIcon';
-import Center from './Center';
-import GraphCard from './GraphCard';
-import Icon from './Icon';
-import Link from './Link';
-import Logo from './Logo';
-import { Step, StepItem, StepLink } from './StepBar';
+import ButtonGroup from "./ButtonGroup";
+import ButtonIcon from "./ButtonIcon";
+import Center from "./Center";
+import GraphCard from "./GraphCard";
+import Icon from "./Icon";
+import Link from "./Link";
+import Logo from "./Logo";
+import { Step, StepItem, StepLink } from "./StepBar";
 import {
-	TableWrapper,
-	TableWrapperBody,
-	TableWrapperFooter,
-} from './TableWrapper';
-import { TargetList, TargetListHeader, TargetListItem } from './Target';
-import { StyledUl, StyledLi, CustomButton } from './ListElements';
-import Text from './Text';
-import ImageIcon from './ImageIcon';
-import Loader from './Loader';
-import VideoCard from './VideoCard'
+  TableWrapper,
+  TableWrapperBody,
+  TableWrapperFooter,
+} from "./TableWrapper";
+import { TargetList, TargetListHeader, TargetListItem } from "./Target";
+import { StyledUl, StyledLi, CustomButton } from "./ListElements";
+import Text from "./Text";
+import ImageIcon from "./ImageIcon";
+import Loader from "./Loader";
+import VideoCard from "./VideoCard";
+import MlModal from "./MlModal";
 
 export {
-	ButtonGroup,
-	ButtonIcon,
-	Center,
-	GraphCard,
-	Icon,
-	Link,
-	Logo,
-	Step,
-	StepItem,
-	StepLink,
-	TableWrapper,
-	TableWrapperBody,
-	TableWrapperFooter,
-	TargetList,
-	TargetListHeader,
-	TargetListItem,
-	Text,
-	StyledUl,
-	StyledLi,
-	CustomButton,
-	ImageIcon,
-	Loader,
-	VideoCard,
+  ButtonGroup,
+  ButtonIcon,
+  Center,
+  GraphCard,
+  Icon,
+  Link,
+  Logo,
+  Step,
+  StepItem,
+  StepLink,
+  TableWrapper,
+  TableWrapperBody,
+  TableWrapperFooter,
+  TargetList,
+  TargetListHeader,
+  TargetListItem,
+  Text,
+  StyledUl,
+  StyledLi,
+  CustomButton,
+  ImageIcon,
+  Loader,
+  VideoCard,
+  MlModal,
 };
