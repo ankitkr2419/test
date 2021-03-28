79b47bf0d467b5bf14e29f1c0138885818798e5e
SankhojjalC <sankhojjalc@gmail.com>
2021-03-29 00:50:31 +0530

    corrected the recipeData input.

--- web-client/src/containers/RecipeListingContainer.jsx
+++ web-client/src/containers/RecipeListingContainer.jsx
@@ -7,44 +7,8 @@ import { recipeListingInitiated } from "action-creators/recipeActionCreators";
 
 const RecipeListingContainer = (props) => {
   const dispatch = useDispatch();
-  // const recipeActionReducer = useSelector((state) => state.recipeActionReducer);
-  // const { recipeData } = recipeActionReducer
-  const recipeData = [
-    {
-      id: "6b7fcfa2-8337-4d79-829a-e9bd486a2de4",
-      name: "covid",
-      description: "Recipe for covid extraction",
-      pos_1: 1,
-      pos_2: 2,
-      pos_3: 3,
-      pos_4: 4,
-      pos_5: 5,
-      pos_cartridge_1: 1,
-      pos_7: 6,
-      pos_cartridge_2: 2,
-      pos_9: 7,
-      process_count: 19,
-      created_at: "2021-03-11T17:34:16.101723Z",
-      updated_at: "2021-03-11T17:34:16.101723Z",
-    },
-    {
-      id: "6b7fcfa2-8337-4d79-829a-e9bd486a2de5",
-      name: "tip_docking",
-      description: "Recipe for tip_docking position on deck",
-      pos_1: 1,
-      pos_2: 2,
-      pos_3: 3,
-      pos_4: 4,
-      pos_5: 5,
-      pos_cartridge_1: 1,
-      pos_7: 6,
-      pos_cartridge_2: 2,
-      pos_9: 7,
-      process_count: 1,
-      created_at: "2021-03-11T17:34:16.21166Z",
-      updated_at: "2021-03-11T17:34:16.21166Z",
-    },
-  ];
+  const recipeActionReducer = useSelector((state) => state.recipeActionReducer);
+  const { recipeData } = recipeActionReducer;
 
   useEffect(() => dispatch(recipeListingInitiated()), [dispatch]);
 
