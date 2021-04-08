87faa265869cf18e40ac768b08d4a74fe19545bd
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-04-08 12:08:18 +0530

    simulator changes for homing

--- plc/simulator/extraction_simulator.go
+++ plc/simulator/extraction_simulator.go
@@ -52,9 +52,8 @@ func (us *ExtractionSimulator) Homing() (string, error) {
 		if err != nil {
 			us.ErrCh <- err
 		}
-		us.WsMsgCh <- fmt.Sprintf("progress_homing_%v", string(wsData))
+		us.WsMsgCh <- fmt.Sprintf("progress_homing%v_%v", us.NameOfDeck(), string(wsData))
 	}
-	us.WsMsgCh <- fmt.Sprintf("success_homing_successfully homed for deck %v", us.name)
 	return "SUCCESS", nil
 }
 
