1e9033d4be393a9103ff8a4bdd6bf01509cdba80
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-04-14 19:34:52 +0530

    simulate Switch On motor is coded

--- plc/simulator/simulate_deck.go
+++ plc/simulator/simulate_deck.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	logger "github.com/sirupsen/logrus"
 	"mylab/cpagent/plc"
+	"time"
 )
 
 func (d *SimulatorDriver) simulateWriteMultipleRegisters(address, quantity uint16, value []byte) (results []byte, err error) {
@@ -131,11 +132,102 @@ func (d *SimulatorDriver) simulateMotor(value uint16) (err error) {
 	return
 }
 
+var motorDone = map[string](chan bool){
+	"A": make(chan bool, 1),
+	"B": make(chan bool, 1),
+}
+
+var sensorDone = map[string](chan bool){
+	"A": make(chan bool, 1),
+	"B": make(chan bool, 1),
+}
+
 func (d *SimulatorDriver) simulateOnMotor() (err error) {
+
+	// Reset D212
+	REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][212]] = 0
+
+	// Pulses Register
+	// If Pulses greater than 0 && Direction is towards Sensor
+	if REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][202]] > 0 &&
+		REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][206]] == plc.TowardsSensor {
+		// Call MonitorSensorCut in a go routine
+		// Only makes sense when we are going towards Sensor
+		go d.monitorSensorCut()
+	}
+
+	// Update Pulses every 100 Millisecond
+	err = d.updatePulses()
+
 	return
 }
 
+func (d *SimulatorDriver) updatePulses() (err error) {
+	motorNum := REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][226]]
+	pulses := REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][200]]
+	speed := REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][202]]
+	currentPulses := uint16(0)
+
+	for {
+		select {
+		case done := <-motorDone[d.DeckName]:
+			logger.Infoln("completion was done ", done, " for deck ", d.DeckName)
+			return
+		case done := <-sensorDone[d.DeckName]:
+			logger.Infoln("sensor has cut", done, " for deck ", d.DeckName)
+			return
+		default:
+			time.Sleep(100 * time.Millisecond)
+			// if motor is OFF then return
+			if plc.OFF == REGISTERS_EXTRACTION[d.DeckName]["M"][plc.MODBUS_EXTRACTION[d.DeckName]["M"][0]] {
+				return
+			}
+			// if motor is changed then return
+			if motorNum != REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][226]] {
+				return
+			}
+
+			// We are updating D212 after every 0.1 second
+			REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][212]] += uint16(float64(speed) * 0.1)
+			currentPulses = REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][212]]
+			if currentPulses > pulses {
+				// D212 updated
+				REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][212]] = pulses
+				// Completion Done
+				REGISTERS_EXTRACTION[d.DeckName]["M"][plc.MODBUS_EXTRACTION[d.DeckName]["M"][1]] = uint16(1)
+				// Completion is monitored here itself
+				motorDone[d.DeckName] <- true
+			}
+		}
+	}
+}
+
+func (d *SimulatorDriver) monitorSensorCut() (err error) {
+
+	// Add position shift coz of D212 as well
+	// shift = D212 Pulses   /    Motor steps
+	for {
+		// putting declaration inside the loop cause what if motor change happens within 100 ms!!
+		deckAndMotor := plc.DeckNumber{Deck: d.DeckName, Number: REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][226]]}
+
+		// if motor is OFF then return
+		if plc.OFF == REGISTERS_EXTRACTION[d.DeckName]["M"][plc.MODBUS_EXTRACTION[d.DeckName]["M"][0]] {
+			return
+		}
+
+		// Check For Sensor Cut
+		shift := float64(REGISTERS_EXTRACTION[d.DeckName]["D"][plc.MODBUS_EXTRACTION[d.DeckName]["D"][212]]) / float64(plc.Motors[deckAndMotor]["steps"])
+		if plc.Positions[deckAndMotor]-shift <= plc.Calibs[deckAndMotor] {
+			REGISTERS_EXTRACTION[d.DeckName]["M"][plc.MODBUS_EXTRACTION[d.DeckName]["M"][2]] = plc.SensorCut
+			sensorDone[d.DeckName] <- true
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
 func (d *SimulatorDriver) simulateOffMotor() (err error) {
+	// No need, its already handled
 	return
 }
 
