33593ffedb1e6b712955aaa321b196a974f49ba8
SankhojjalC <sankhojjalc@gmail.com>
2021-04-20 16:49:01 +0530

    added footer

---
+++ web-client/src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import DeckCard from "shared-components/DeckCard";
+
+import { DECKNAME } from "appConstants";
+
+const Footer = (props) => {
+  const { deckName } = props;
+
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+        
+    </div>
+  );
+};
+
+export default Footer;
