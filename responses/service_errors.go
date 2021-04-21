b6ce3fc7da5049c5b3fd85a1499c39df3112e09b
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-04-21 15:39:24 +0530

    Update related data added

--- responses/service_errors.go
+++ responses/service_errors.go
@@ -9,6 +9,7 @@ var(
 	PiercingValidationError = fmt.Errorf("error validating piercing record")
 	PiercingCreateError = fmt.Errorf("error creating piercing record")
 	PiercingFetchError = fmt.Errorf("error fetching piercing record")
+	PiercingUpdateError = fmt.Errorf("error updating piercing record")
 	UUIDParseError = fmt.Errorf("error parsing uuid")
 )
 
--- responses/service_info.go
+++ responses/service_info.go
@@ -6,4 +6,5 @@ import(
 var (
 	PiercingCreateSuccess = "piercing record created successfully"
 	PiercingFetchSuccess = "piercing record fetched successfully"
+	PiercingUpdateSuccess = "piercing record updated successfully"
 )
