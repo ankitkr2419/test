1651284a35adb5ecf9b78a407bafa434249b75e6
Paramita Majumdar <sonamajumdar2012@gmail.com>
2021-04-22 12:06:26 +0530

    added if not exists clause

--- migrations/1589352100_create_target_table.up.sql
+++ migrations/1589352100_create_target_table.up.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 
-CREATE TABLE targets (
+CREATE TABLE IF NOT EXISTS targets (
   id uuid primary key default uuid_generate_v4(),
 	name varchar(50),
   dye_id uuid,
