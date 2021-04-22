43006140f2d197ef93e064264c643ac66bb5ff27
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-04-22 09:45:16 +0530

    Aspire Dispense errors and infos added

--- responses/service_errors.go
+++ responses/service_errors.go
@@ -1,15 +1,22 @@
 package responses
 
-import(
+import (
 	"fmt"
 )
 
-var(
-	PiercingDecodeError = fmt.Errorf("error decoding piercing record")
+var (
+	PiercingDecodeError     = fmt.Errorf("error decoding piercing record")
 	PiercingValidationError = fmt.Errorf("error validating piercing record")
-	PiercingCreateError = fmt.Errorf("error creating piercing record")
-	PiercingFetchError = fmt.Errorf("error fetching piercing record")
-	PiercingUpdateError = fmt.Errorf("error updating piercing record")
+	PiercingCreateError     = fmt.Errorf("error creating piercing record")
+	PiercingFetchError      = fmt.Errorf("error fetching piercing record")
+	PiercingUpdateError     = fmt.Errorf("error updating piercing record")
+
+	AspireDispenseDecodeError     = fmt.Errorf("error decoding aspire dispense record")
+	AspireDispenseValidationError = fmt.Errorf("error validating aspire dispense record")
+	AspireDispenseCreateError     = fmt.Errorf("error creating aspire dispense record")
+	AspireDispenseFetchError      = fmt.Errorf("error fetching aspire dispense record")
+	AspireDispenseUpdateError     = fmt.Errorf("error updating aspire dispense record")
+
 	UUIDParseError = fmt.Errorf("error parsing uuid")
 )
 
--- responses/service_info.go
+++ responses/service_info.go
@@ -1,10 +1,13 @@
 package responses
 
-import(
-)
+import ()
 
 var (
 	PiercingCreateSuccess = "piercing record created successfully"
-	PiercingFetchSuccess = "piercing record fetched successfully"
+	PiercingFetchSuccess  = "piercing record fetched successfully"
 	PiercingUpdateSuccess = "piercing record updated successfully"
+
+	AspireDispenseCreateSuccess = "aspire dispense record created successfully"
+	AspireDispenseFetchSuccess  = "aspire dispense record fetched successfully"
+	AspireDispenseUpdateSuccess = "aspire dispense record updated successfully"
 )
