5b4da0d1d27a010f8675a4ce5d592ed8686b328a
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-04-22 10:12:10 +0530

    errors and info for other processes added as well

--- responses/service_errors.go
+++ responses/service_errors.go
@@ -17,6 +17,42 @@ var (
 	AspireDispenseFetchError      = fmt.Errorf("error fetching aspire dispense record")
 	AspireDispenseUpdateError     = fmt.Errorf("error updating aspire dispense record")
 
+	DelayDecodeError     = fmt.Errorf("error decoding delay record")
+	DelayValidationError = fmt.Errorf("error validating delay record")
+	DelayCreateError     = fmt.Errorf("error creating delay record")
+	DelayFetchError      = fmt.Errorf("error fetching delay record")
+	DelayUpdateError     = fmt.Errorf("error updating delay record")
+
+	AttachDetachDecodeError     = fmt.Errorf("error decoding attach detach record")
+	AttachDetachValidationError = fmt.Errorf("error validating attach detach record")
+	AttachDetachCreateError     = fmt.Errorf("error creating attach detach record")
+	AttachDetachFetchError      = fmt.Errorf("error fetching attach detach record")
+	AttachDetachUpdateError     = fmt.Errorf("error updating attach detach record")
+
+	ShakingDecodeError     = fmt.Errorf("error decoding shaking record")
+	ShakingValidationError = fmt.Errorf("error validating shaking record")
+	ShakingCreateError     = fmt.Errorf("error creating shaking record")
+	ShakingFetchError      = fmt.Errorf("error fetching shaking record")
+	ShakingUpdateError     = fmt.Errorf("error updating shaking record")
+
+	HeatingDecodeError     = fmt.Errorf("error decoding heating record")
+	HeatingValidationError = fmt.Errorf("error validating heating record")
+	HeatingCreateError     = fmt.Errorf("error creating heating record")
+	HeatingFetchError      = fmt.Errorf("error fetching heating record")
+	HeatingUpdateError     = fmt.Errorf("error updating heating record")
+
+	TipDockingDecodeError     = fmt.Errorf("error decoding tip docking record")
+	TipDockingValidationError = fmt.Errorf("error validating tip docking record")
+	TipDockingCreateError     = fmt.Errorf("error creating tip docking record")
+	TipDockingFetchError      = fmt.Errorf("error fetching tip docking record")
+	TipDockingUpdateError     = fmt.Errorf("error updating tip docking record")
+
+	TipOperationDecodeError     = fmt.Errorf("error decoding tip operation record")
+	TipOperationValidationError = fmt.Errorf("error validating tip operation record")
+	TipOperationCreateError     = fmt.Errorf("error creating tip operation record")
+	TipOperationFetchError      = fmt.Errorf("error fetching tip operation record")
+	TipOperationUpdateError     = fmt.Errorf("error updating tip operation record")
+
 	UUIDParseError = fmt.Errorf("error parsing uuid")
 )
 
--- responses/service_info.go
+++ responses/service_info.go
@@ -10,4 +10,28 @@ var (
 	AspireDispenseCreateSuccess = "aspire dispense record created successfully"
 	AspireDispenseFetchSuccess  = "aspire dispense record fetched successfully"
 	AspireDispenseUpdateSuccess = "aspire dispense record updated successfully"
+
+	DelayCreateSuccess = "delay record created successfully"
+	DelayFetchSuccess  = "delay record fetched successfully"
+	DelayUpdateSuccess = "delay record updated successfully"
+
+	AttachDetachCreateSuccess = "attach detach record created successfully"
+	AttachDetachFetchSuccess  = "attach detach record fetched successfully"
+	AttachDetachUpdateSuccess = "attach detach record updated successfully"
+
+	ShakingCreateSuccess = "shaking record created successfully"
+	ShakingFetchSuccess  = "shaking record fetched successfully"
+	ShakingUpdateSuccess = "shaking record updated successfully"
+
+	HeatingCreateSuccess = "heating record created successfully"
+	HeatingFetchSuccess  = "heating record fetched successfully"
+	HeatingUpdateSuccess = "heating record updated successfully"
+
+	TipDockingCreateSuccess = "tip docking record created successfully"
+	TipDockingFetchSuccess  = "tip docking record fetched successfully"
+	TipDockingUpdateSuccess = "tip docking record updated successfully"
+
+	TipOperationCreateSuccess = "tip operation record created successfully"
+	TipOperationFetchSuccess  = "tip operation record fetched successfully"
+	TipOperationUpdateSuccess = "tip operation record updated successfully"
 )
