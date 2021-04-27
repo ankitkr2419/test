077cbef7379474c9750b1557f097bd30e8a583cf
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-04-27 13:21:11 +0530

    runNextStep sync.Map added in utils

--- plc/utils.go
+++ plc/utils.go
@@ -73,6 +73,7 @@ const (
 
 var wrotePulses, executedPulses, aborted, paused, homed sync.Map
 var runInProgress, magnetState, timerInProgress, heaterInProgress, uvLightInProgress, syringeModuleState, shakerInProgress sync.Map
+var runNextStep sync.Map
 
 // Special variables for both deck operation
 var BothDeckHomingInProgress bool
@@ -107,6 +108,9 @@ func LoadUtils() {
 
 	homed.Store("A", false)
 	homed.Store("B", false)
+
+	runNextStep.Store("A", false)
+	runNextStep.Store("B", false)
 
 	BothDeckHomingInProgress = false
 	homingPercent.Store("A", float64(0))
