c61ef06c1f676db277b54b8c0e5616e2a687ab25
Aryan Jain <aryanjn807@gmail.com>
2021-05-01 19:13:34 +0530

    optimized code and added tip discard

--- web-client/src/components/AppFooter/index.jsx
+++ web-client/src/components/AppFooter/index.jsx
@@ -23,24 +23,15 @@ import {
 } from "action-creators/cleanUpActionCreators";
 import { MlModal } from "shared-components";
 import { loginReset } from "action-creators/loginActionCreators";
+import TipDiscardModal from "components/modals/TipDiscardModal";
+import { discardTipAndHomingActionInitiated } from "action-creators/homingActionCreators";
 
 const AppFooter = (props) => {
   const dispatch = useDispatch();
 
-  const [
-    isConfirmationModalVisibleDeckA,
-    setIsConfirmationModalVisibleDeckA,
-  ] = useState(false);
-  const [
-    isConfirmationModalVisibleDeckB,
-    setIsConfirmationModalVisibleDeckB,
-  ] = useState(false);
-
-  const [abortConfirmationModalA, setAbortConfirmationModalA] = useState(false);
-  const [abortConfirmationModalB, setAbortConfirmationModalB] = useState(false);
-
   const [confirmAbortModal, setConfirmAbortModal] = useState(false);
   const [confirmDoneModal, setConfirmDoneModal] = useState(false);
+  const [tipDiscardModal, setTipDiscardModal] = useState(false);
   const [deckName, setDeckName] = useState("");
 
   //login reducer
@@ -162,6 +153,7 @@ const AppFooter = (props) => {
     }
   };
 
+  // RUN
   const handleRunActionDeckA = () => {
     let recipeReducerData = recipeActionReducerForDeckA;
 
@@ -203,6 +195,7 @@ const AppFooter = (props) => {
     }
   };
 
+  //PAUSE
   const handlePauseActionDeckA = () => {
     let recipeReducerData = recipeActionReducerForDeckA;
 
@@ -222,6 +215,7 @@ const AppFooter = (props) => {
     }
   };
 
+  // RESUME
   const handleResumeActionDeckA = () => {
     let recipeReducerData = recipeActionReducerForDeckA;
 
@@ -242,25 +236,95 @@ const AppFooter = (props) => {
     }
   };
 
-  const handleDoneActionDeckA = () => {
+  // CANCEL
+  const handleCancelActionDeckA = () => {
     let recipeReducerData = recipeActionReducerForDeckA;
+
     if (recipeReducerData.showProcess) {
-      toggleComfirmationModal(DECKNAME.DeckA);
+      dispatch(runRecipeReset(deckName));
     } else {
-      setDeckName(DECKNAME.DeckA);
-      setConfirmDoneModal(!confirmDoneModal);
+      dispatch(runCleanUpActionReset({ deckName: DECKNAME.DeckA }));
+    }
+  };
+  const handleCancelActionDeckB = () => {
+    let recipeReducerData = recipeActionReducerForDeckB;
+
+    if (recipeReducerData.showProcess) {
+      dispatch(runRecipeReset(deckName));
+    } else {
+      dispatch(runCleanUpActionReset({ deckName: DECKNAME.DeckB }));
     }
   };
 
-  const handleDoneActionDeckB = () => {
-    let recipeReducerData = recipeActionReducerForDeckB;
-    if (recipeReducerData.showProcess) {
-      toggleComfirmationModal(DECKNAME.DeckB);
-    } else {
-      setDeckName(DECKNAME.DeckB);
-      setConfirmDoneModal(!confirmDoneModal);
-    }
+  //ABORT
+  const handleAbortActionDeckA = () => {
+    setDeckName(DECKNAME.DeckA);
+    setConfirmAbortModal(!confirmAbortModal);
   };
+
+  const handleAbortActionDeckB = () => {
+    setDeckName(DECKNAME.DeckB);
+    setConfirmAbortModal(!confirmAbortModal);
+  };
+
+  const handleAbortModalDeckA = () => {
+    let recipeReducerData = recipeActionReducerForDeckA;
+
+    if (recipeReducerData.showProcess) {
+      dispatch(abortRecipeInitiated(DECKNAME.DeckA));
+      setTipDiscardModal(!tipDiscardModal);
+    } else {
+      dispatch(abortCleanUpActionInitiated({ deckName: DECKNAME.DeckAShort }));
+      dispatch(loginReset(DECKNAME.DeckA));
+    }
+    setConfirmAbortModal(!confirmAbortModal);
+  };
+
+  const handleAbortModalDeckB = () => {
+    let recipeReducerData = recipeActionReducerForDeckB;
+
+    if (recipeReducerData.showProcess) {
+      dispatch(abortRecipeInitiated(DECKNAME.DeckB));
+      setTipDiscardModal(!tipDiscardModal);
+    } else {
+      dispatch(abortCleanUpActionInitiated({ deckName: DECKNAME.DeckBShort }));
+      dispatch(loginReset(DECKNAME.DeckB));
+    }
+    setConfirmAbortModal(!confirmAbortModal);
+  };
+
+  const toggleTipDiscardModal = (discardTip) => {
+    if (deckName === DECKNAME.DeckA) {
+      dispatch(
+        discardTipAndHomingActionInitiated({
+          deckName: DECKNAME.DeckAShort,
+          discardTip: discardTip,
+        })
+      );
+      dispatch(loginReset(DECKNAME.DeckA));
+    } else {
+      dispatch(
+        discardTipAndHomingActionInitiated({
+          deckName: DECKNAME.DeckBShort,
+          discardTip: discardTip,
+        })
+      );
+      dispatch(loginReset(DECKNAME.DeckB));
+    }
+    setTipDiscardModal(!tipDiscardModal);
+  };
+
+  //DONE
+  const handleDoneActionDeckA = () => {
+    setDeckName(DECKNAME.DeckA);
+    setConfirmDoneModal(!confirmDoneModal);
+  };
+
+  const handleDoneActionDeckB = () => {
+    setDeckName(DECKNAME.DeckB);
+    setConfirmDoneModal(!confirmDoneModal);
+  };
+
   const handleDoneModalDeckA = () => {
     let recipeReducerData = recipeActionReducerForDeckA;
 
@@ -285,96 +349,17 @@ const AppFooter = (props) => {
     setConfirmDoneModal(!confirmDoneModal);
   };
 
-  const handleCancelActionDeckA = () => {
-    let recipeReducerData = recipeActionReducerForDeckA;
-
-    if (recipeReducerData.showProcess) {
-      dispatch(runRecipeReset(deckName));
-    } else {
-      dispatch(runCleanUpActionReset({ deckName: DECKNAME.DeckA }));
-    }
-  };
-  const handleCancelActionDeckB = () => {
-    let recipeReducerData = recipeActionReducerForDeckB;
-
-    if (recipeReducerData.showProcess) {
-      dispatch(runRecipeReset(deckName));
-    } else {
-      dispatch(runCleanUpActionReset({ deckName: DECKNAME.DeckB }));
-    }
-  };
-
-  const handleAbortActionDeckA = () => {
-    let recipeReducerData = recipeActionReducerForDeckA;
-
-    if (recipeReducerData.showProcess) {
-      toggleAbortConfirmationModal(DECKNAME.DeckA);
-    } else {
-      setConfirmAbortModal(!confirmAbortModal);
-      setDeckName(DECKNAME.DeckA);
-    }
-  };
-
-  const handleAbortActionDeckB = () => {
-    let recipeReducerData = recipeActionReducerForDeckA;
-
-    if (recipeReducerData.showProcess) {
-      toggleAbortConfirmationModal(DECKNAME.DeckB);
-    } else {
-      setConfirmAbortModal(!confirmAbortModal);
-      setDeckName(DECKNAME.DeckB);
-    }
-  };
-
-  const handleAbortModalDeckA = () => {
-    let recipeReducerData = recipeActionReducerForDeckA;
-
-    if (recipeReducerData.showProcess) {
-    } else {
-      setConfirmAbortModal(!confirmAbortModal);
-      dispatch(abortCleanUpActionInitiated({ deckName: DECKNAME.DeckAShort }));
-      dispatch(loginReset(DECKNAME.DeckA));
-    }
-  };
-
-  const toggleComfirmationModal = (deckName) => {
-    if (deckName === DECKNAME.DeckA) {
-      setIsConfirmationModalVisibleDeckA(!isConfirmationModalVisibleDeckA);
-    } else {
-      setIsConfirmationModalVisibleDeckB(!isConfirmationModalVisibleDeckB);
-    }
-  };
-
-  const toggleAbortConfirmationModal = (deckName) => {
-    if (deckName === DECKNAME.DeckA) {
-      setAbortConfirmationModalA(!abortConfirmationModalA);
-    } else {
-      setAbortConfirmationModalB(!abortConfirmationModalB);
-    }
-  };
-
-  const onAbortConfirmed = (deckName) => {
-    toggleAbortConfirmationModal(deckName);
-    dispatch(abortRecipeInitiated({ deckName }));
-  };
-
-  const onNextClickedAfterDoneRecipe = (deckName) => {
-    dispatch(loginReset(deckName));
-  };
-
-  const handleAbortModalDeckB = () => {
-    setConfirmAbortModal(!confirmAbortModal);
-    dispatch(abortCleanUpActionInitiated({ deckName: DECKNAME.DeckBShort }));
-    dispatch(loginReset(DECKNAME.DeckB));
-  };
-
   return (
     <div className="d-flex justify-content-center align-items-center">
       {confirmAbortModal && (
         <MlModal
           isOpen={confirmAbortModal}
           textHead={deckName}
-          textBody={MODAL_MESSAGE.abortCleanupConfirmation}
+          textBody={
+            recipeActionReducerForDeckA.showProcess
+              ? MODAL_MESSAGE.abortConfirmation
+              : MODAL_MESSAGE.abortCleanupConfirmation
+          }
           successBtn={MODAL_BTN.yes}
           failureBtn={MODAL_BTN.no}
           handleSuccessBtn={
@@ -388,49 +373,15 @@ const AppFooter = (props) => {
         />
       )}
 
-      <MlModal
-        isOpen={isConfirmationModalVisibleDeckA}
-        textHead={DECKNAME.DeckA}
-        textBody={`Experiment was successful`}
-        handleSuccessBtn={() => onNextClickedAfterDoneRecipe(DECKNAME.DeckA)}
-        handleCrossBtn={() => toggleComfirmationModal(DECKNAME.DeckA)}
-        successBtn={MODAL_BTN.next}
-      />
-
-      <MlModal
-        isOpen={isConfirmationModalVisibleDeckB}
-        textHead={DECKNAME.DeckB}
-        textBody={`Experiment was successful`}
-        handleSuccessBtn={() => onNextClickedAfterDoneRecipe(DECKNAME.DeckB)}
-        handleCrossBtn={() => toggleComfirmationModal(DECKNAME.DeckB)}
-        successBtn={MODAL_BTN.next}
-      />
-
-      <MlModal
-        isOpen={abortConfirmationModalA}
-        textHead={DECKNAME.DeckA}
-        textBody={MODAL_MESSAGE.abortConfirmation}
-        handleSuccessBtn={() => onAbortConfirmed(DECKNAME.DeckA)}
-        handleCrossBtn={() => toggleAbortConfirmationModal(DECKNAME.DeckA)}
-        successBtn={MODAL_BTN.yes}
-        failureBtn={MODAL_BTN.no}
-      />
-
-      <MlModal
-        isOpen={abortConfirmationModalB}
-        textHead={DECKNAME.DeckB}
-        textBody={MODAL_MESSAGE.abortConfirmation}
-        handleSuccessBtn={() => onAbortConfirmed(DECKNAME.DeckB)}
-        handleCrossBtn={() => toggleAbortConfirmationModal(DECKNAME.DeckB)}
-        successBtn={MODAL_BTN.yes}
-        failureBtn={MODAL_BTN.no}
-      />
-
       {confirmDoneModal && (
         <MlModal
           isOpen={confirmDoneModal}
           textHead={deckName}
-          textBody={MODAL_MESSAGE.uvSuccess}
+          textBody={
+            recipeActionReducerForDeckA.showProcess
+              ? MODAL_MESSAGE.experimentSuccess
+              : MODAL_MESSAGE.uvSuccess
+          }
           successBtn={MODAL_BTN.next}
           handleSuccessBtn={
             deckName === DECKNAME.DeckA
@@ -442,6 +393,14 @@ const AppFooter = (props) => {
           }}
         />
       )}
+
+      {
+        <TipDiscardModal
+          isOpen={tipDiscardModal}
+          handleSuccessBtn={toggleTipDiscardModal}
+          deckName={deckName}
+        />
+      }
 
       {/**Deck A */}
       <DeckCard
@@ -496,11 +455,6 @@ const AppFooter = (props) => {
                 JSON.parse(cleanUpReducerForDeckA.cleanUpData).progress
             : 0
         }
-        progressPercentComplete={
-          recipeActionReducerForDeckA.runRecipeInProgress
-            ? recipeActionReducerForDeckA.runRecipeInProgress.progress
-            : 0
-        }
         showCleanUp={
           isDeckALoggedIn ? cleanUpReducerForDeckA.showCleanUp : false
         }
@@ -522,8 +476,8 @@ const AppFooter = (props) => {
         }
         processNumber={
           recipeActionReducerForDeckB.runRecipeInProgress
-            ? JSON.parse(recipeActionReducerForDeckB.runRecipeInProgress)
-                .operation_details.current_step
+            ? recipeActionReducerForDeckB.runRecipeInProgress.operation_details
+                .current_step
             : 0
         }
         processTotal={
@@ -559,8 +513,7 @@ const AppFooter = (props) => {
           isDeckBLoggedIn
             ? recipeActionReducerForDeckB.showProcess
               ? recipeActionReducerForDeckB.runRecipeInProgress &&
-                JSON.parse(recipeActionReducerForDeckB.runRecipeInProgress)
-                  .progress
+                recipeActionReducerForDeckB.runRecipeInProgress.progress
               : cleanUpReducerForDeckB.cleanUpData &&
                 JSON.parse(cleanUpReducerForDeckB.cleanUpData).progress
             : 0
