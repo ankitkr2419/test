633ceb6046731b8cdc78dc9474f02791963e0aaa
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-05-05 17:56:43 +0530

    delay process gets created as part of transaction now

--- db/delay.go
+++ db/delay.go
@@ -13,7 +13,7 @@ import (
 type Delay struct {
 	ID        uuid.UUID `db:"id" json:"id"`
 	DelayTime int64     `db:"delay_time" json:"delay_time" validate:"required"`
-	ProcessID uuid.UUID `db:"process_id" json:"process_id" validate:"required"`
+	ProcessID uuid.UUID `db:"process_id" json:"process_id"`
 	CreatedAt time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
 }
@@ -42,42 +42,55 @@ func (s *pgStore) ShowDelay(ctx context.Context, id uuid.UUID) (delay Delay, err
 	return
 }
 
-func (s *pgStore) CreateDelay(ctx context.Context, d Delay) (createdD Delay, err error) {
+func (s *pgStore) CreateDelay(ctx context.Context, ad Delay, recipeID uuid.UUID) (createdAD Delay, err error) {
 	var tx *sql.Tx
-
-	//update the process name before record creation
-	err = s.updateProcessName(ctx, d.ProcessID, "Delay", d)
-	if err != nil {
-		logger.WithField("err", err.Error()).Errorln(responses.DelayUpdateNameError)
-		return
-	}
-
 	tx, err = s.db.BeginTx(ctx, nil)
 	if err != nil {
 		logger.WithField("err:", err.Error()).Errorln(responses.DelayInitiateDBTxError)
-		return Delay{}, err
+		return
 	}
 
-	createdD, err = s.createDelay(ctx, tx, d)
-	// failures are already logged
-	// Commit the transaction else won't be able to Show
-
-	// End the transaction in defer call
 	defer func() {
 		if err != nil {
 			tx.Rollback()
+			logger.Errorln(responses.DelayCreateError)
 			return
 		}
 		tx.Commit()
-		createdD, err = s.ShowDelay(ctx, createdD.ProcessID)
+		createdAD, err = s.ShowDelay(ctx, createdAD.ProcessID)
 		if err != nil {
-			logger.Infoln("Error Creating Delay process")
+			logger.Errorln(responses.DelayFetchError)
 			return
 		}
-		logger.Infoln("Created Delay Process: ", createdD)
+		logger.Infoln(responses.DelayCreateSuccess, createdAD)
 		return
 	}()
 
+	// Get highest sequence number
+	// NOTE: failure already logged in internal calls
+
+	highestSeqNum, err := s.getProcessCount(ctx, tx, recipeID)
+	if err != nil {
+		return
+	}
+	
+	process, err := s.processOperation(ctx, name, DelayProcess, ad, Process{})
+	if err != nil {
+		return
+	}
+	// process has only a valid name
+	process.SequenceNumber = highestSeqNum + 1
+	process.Type = string(DelayProcess)
+	process.RecipeID = recipeID
+
+	// create the process
+	process, err = s.createProcess(ctx, tx, process)
+	if err != nil {
+		return
+	}
+
+	ad.ProcessID = process.ID
+	createdAD, err = s.createDelay(ctx, tx, ad)
 	return
 }
 
