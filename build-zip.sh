ea0fa38c0ffa916ae0929cccb1646831e8b78e19
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-05-11 17:06:46 +0530

    build-zip updated

--- build-zip.sh
+++ build-zip.sh
@@ -1,9 +1,12 @@
-read -p "please input the zip version: " ver
+#!/bin/bash
+read -p "please input the artifact version: " ver
 echo "creting a temporary directory named ${ver}"
 mkdir ${ver}
 echo "copying migrations,conf,cpagent and installation.sh to ${ver}"
 cp -r migrations conf cpagent installation.sh ${ver}
+echo "deleting old artifact.zip if any"
+rm -f artifact.zip
 echo "creating artifact.zip"
-zip artifact.zip ${ver}/*
-echo "deleting ${ver} folder"
-rm -r ${ver}
+zip -r artifact.zip ${ver}
+echo "deleting folder ${ver}"
+rm -rf ${ver}
