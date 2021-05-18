444500b8ac7417e636d52843fafa95717ace4c13
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-05-18 12:07:55 +0530

    installation script updated

--- utils/installation.sh
+++ utils/installation.sh
@@ -4,10 +4,27 @@ export curr_ver=`pwd`
 if [[ -f $HOME/cpagent/old_version.txt ]]; then
         export old_ver=$(cat $HOME/cpagent/old_version.txt| head -1)
 fi
+
+safe_to_upgrade () {
+
+while [ "$state" -ne 200 ]
+do
+        echo -e "\n\n\t: Not safe to upgrade, Run in progress, waiting 5 sec. :"
+        sleep 5
+        
+	export  state=$(curl -o /dev/null -s -w "%{http_code}\n" --location --request GET '0.0.0.0:33001/safe-to-upgrade' --header 'Accept: application/vnd.MyLabDiscoveries.v1')
+
+done
+
+echo -e "\n\n\t: Safe to upgrade :"
+echo -e "\n\n\t: Progressing .. : "
+}
+
+
 upgrade() {
-        echo "This is upgrade function  :"
-        echo "stoping cpagent service "
-        echo "admin" | sudo -s systemctl stop cpagent
+        echo -e "\n\t: Started with instllation procedure :"
+        echo -e "\n\t: stoping cpagent service :"
+        echo "admin" | sudo -S systemctl stop cpagent
         if [[ -f $HOME/cpagent/old_version2.txt ]]; then
                 rm $HOME/cpagent/old_version2.txt
         fi
@@ -19,50 +36,76 @@ upgrade() {
         fi
         echo "$curr_ver" > $HOME/cpagent/new_version.txt
         if [[ -L $HOME/cpagent/current ]]; then
-                echo "Symlink is present"
+                echo -e "\n\t: Symlink is present :"
                 rm $HOME/cpagent/current
                 ln -sf $curr_ver $HOME/cpagent/current
         else
-                echo "Symlink current not exists, creating symlink with name current"
+                echo -e "\n\t Symlink current not exists, creating symlink with name current :"
                 ln -sf $curr_ver $HOME/cpagent/current
         fi
         ### Run migrations
         cd $HOME/cpagent/current
+	echo -e "\n\t Running migrations :"
         ./cpagent migrate & pid=$!
         wait $pid
         #### Run go binary or start cpagent service
         #echo "admin" | nohup sudo -S ./cpagent start --plc simulator &#
-        echo "admin" | sudo systemctl start cpagent
+	echo -e "\n\t Starting cpagent service : "
+        echo "admin" | sudo -S systemctl start cpagent
         #kill $(ps aux  |grep gedit |grep -v grep |awk {'print $2'})
 }
 revert() {
-        echo "This is revert function :"
-        echo "stop running process"
+        echo -e "\n\t Started with revert procedure :"
+        echo -e "\n\t stoping cpagent service :"
+        
+        echo "admin" | sudo -S systemctl stop cpagent
         if [[ -L $HOME/cpagent/current ]]; then
-                echo "Symlink is present"
+                echo -e "\n\t Symlink is present :"
                 rm $HOME/cpagent/current
 
                 ln -sf ${old_ver} $HOME/cpagent/current
         else
-                echo "Symlink current not exists, creating with name current"
+                echo -e "\n\t Symlink current not exists, creating symlink with name current :"
                 ln -sf ${old_ver} $HOME/cpagent/current
         fi
+	### Run migrations
+        cd $HOME/cpagent/current
+	echo -e "\n\t Running migrations :"
+        ./cpagent migrate & pid=$!
+        wait $pid
+        #### Run go binary or start cpagent service
+        #echo "admin" | nohup sudo -S ./cpagent start --plc simulator &#
+	echo -e "\n\t Starting cpagent service : "
+        echo "admin" | sudo -S systemctl start cpagent
+        #kill $(ps aux  |grep gedit |grep -v grep |awk {'print $2'})
+
 }
 ##### Main #####
 
 if [[ ! -d $HOME/cpagent/release ]]; then
         mkdir $HOME/cpagent/release
 fi
-if [[ ! -d $HOME/logs ]]; then
-        mkdir -p $HOME/logs
+if [[ ! -d $HOME/cpagent/logs ]]; then
+        mkdir -p $HOME/cpagent/logs
 fi
 if [[ ! -d $HOME/logs/$log_dir ]]; then
         mkdir -p $HOME/logs/$log_dir
 fi
-#if [[ ! -d mylab/status ]]; then
-#        mkdir -p $HOME/status
-#fi
-exec > >(tee "mylab/logs/${log_dir}/installation_$(date +"%m%d%Y-%T").log") 2>&1
+
+exec > >(tee "$HOME/cpagent/logs/${log_dir}/installation_$(date +"%m%d%Y-%T").log") 2>&1
+
+export state=$(curl -o /dev/null -s -w "%{http_code}\n" --location --request GET '0.0.0.0:33001/safe-to-upgrade' --header 'Accept: application/vnd.MyLabDiscoveries.v1')
+
+if [[ $state -eq 417 ]]; then
+        echo -e "\n\n\t: Run is in progress :"
+        safe_to_upgrade
+fi
+
+if [[ $state -eq 000 ]]; then
+        echo -e "\n\n\t: Cpagent is in stop state : "
+        echo -e "\n\n\t: Progressing .. : "
+fi
+
 
 if [[ ${action} == "upgrade" ]]; then
 
