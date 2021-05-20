a7ce9937201ca801a8931ba4461f14f8b0257042
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-05-20 10:26:35 +0530

    cleanup http updated for response Code And Msg usage and test cases kickoff

--- service/cleanup_http.go
+++ service/cleanup_http.go
@@ -1,63 +1,53 @@
 package service
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"mylab/cpagent/responses"
+
+	logger "github.com/sirupsen/logrus"
 )
 
 func discardBoxCleanupHandler(deps Dependencies) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 
-		var response string
 		var err error
 
 		vars := mux.Vars(req)
 		deck := vars["deck"]
-		switch deck {
-		case "A", "B":
-			response, err = singleDeckOperation(deps, deck, "DiscardBoxCleanup")
-		default:
-			err = fmt.Errorf("Check your deck name")
+
+		_, err = singleDeckOperation(deps, deck, "DiscardBoxCleanup")
+		if err != nil {
+			logger.Errorln(err.Error())
+			responseCodeAndMsg(rw, http.StatusInternalServerError, ErrObj{Err: err.Error(), Deck: deck})
+			return
 		}
 
-		if err != nil {
-			fmt.Fprintf(rw, err.Error())
-			fmt.Println(err.Error())
-			rw.WriteHeader(http.StatusInternalServerError)
-		} else {
-			rw.Header().Add("Content-Type", "application/json")
-			rw.Write([]byte(fmt.Sprintf(`{"msg":%v,"deck":"%v"}`, response, deck)))
-			rw.WriteHeader(http.StatusOK)
-		}
+		logger.Infoln(responses.DiscardBoxMovedSuccess)
+		responseCodeAndMsg(rw, http.StatusInternalServerError, MsgObj{Msg: responses.DiscardBoxMovedSuccess, Deck: deck})
+		return
 	})
 }
 
 func restoreDeckHandler(deps Dependencies) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 
-		var response string
 		var err error
 
 		vars := mux.Vars(req)
 		deck := vars["deck"]
-		switch deck {
-		case "A", "B":
-			response, err = singleDeckOperation(deps, deck, "RestoreDeck")
-		default:
-			err = fmt.Errorf("Check your deck name")
+
+		_, err = singleDeckOperation(deps, deck, "RestoreDeck")
+		if err != nil {
+			logger.Errorln(err.Error())
+			responseCodeAndMsg(rw, http.StatusInternalServerError, ErrObj{Err: err.Error(), Deck: deck})
+			return
 		}
 
-		if err != nil {
-			fmt.Fprintf(rw, err.Error())
-			fmt.Println(err.Error())
-			rw.WriteHeader(http.StatusInternalServerError)
-		} else {
-			rw.Header().Add("Content-Type", "application/json")
-			rw.Write([]byte(fmt.Sprintf(`{"msg":%v,"deck":"%v"}`, response, deck)))
-			rw.WriteHeader(http.StatusOK)
-		}
+		logger.Infoln(responses.RestoreDeckSuccess)
+		responseCodeAndMsg(rw, http.StatusInternalServerError, MsgObj{Msg: responses.RestoreDeckSuccess, Deck: deck})
+		return
 	})
 }
 
@@ -69,16 +59,9 @@ func uvLightHandler(deps Dependencies) http.HandlerFunc {
 
 		uvTime := vars["time"]
 
-		switch deck {
-		case "A", "B":
-			rw.Header().Add("Content-Type", "application/json")
-			rw.WriteHeader(http.StatusOK)
-			rw.Write([]byte(fmt.Sprintf(`{"msg":"uv light clean up in progress","deck":"%v"}`, deck)))
-			go deps.PlcDeck[deck].UVLight(uvTime)
-		default:
-			err := fmt.Errorf("Check your deck name")
-			deps.WsErrCh <- err
-		}
-
+		go deps.PlcDeck[deck].UVLight(uvTime)
+		logger.Infoln(responses.UVCleanupProgress)
+		responseCodeAndMsg(rw, http.StatusOK, MsgObj{Msg: responses.UVCleanupProgress, Deck: deck})
+		return
 	})
 }
