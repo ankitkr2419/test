54a5c4c67b329747dd9ae0d29067462e590e87f2
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-05-27 09:09:36 +0530

    safe to upgrade success test case coded

--- service/urgrade_http_test.go
+++ service/urgrade_http_test.go
@@ -33,3 +33,28 @@ func (suite *UpgradeHandlerTestSuite) SetupTest() {
 func TestUpgradeTestSuite(t *testing.T) {
 	suite.Run(t, new(UpgradeHandlerTestSuite))
 }
+
+func (suite *UpgradeHandlerTestSuite) TestSafeToUpgradeSuccess() {
+	// Deck A
+	suite.plcDeck[deckA].(*plc.PLCMockStore).On("IsRunInProgress").Return(false).Once()
+	suite.plcDeck[deckA].(*plc.PLCMockStore).On("SetRunInProgress").Return().Maybe()
+	suite.plcDeck[deckA].(*plc.PLCMockStore).On("ResetRunInProgress").Return().Maybe()
+	// Deck B
+	suite.plcDeck[deckB].(*plc.PLCMockStore).On("IsRunInProgress").Return(false).Once()
+	suite.plcDeck[deckB].(*plc.PLCMockStore).On("SetRunInProgress").Return().Maybe()
+	suite.plcDeck[deckB].(*plc.PLCMockStore).On("ResetRunInProgress").Return().Maybe()
+
+	recorder := makeHTTPCall(http.MethodGet,
+		"/safe-to-upgrade",
+		"/safe-to-upgrade",
+		"",
+		safeToUpgradeHandler(Dependencies{Store: suite.dbMock, PlcDeck: suite.plcDeck}),
+	)
+
+	output, _ := json.Marshal(MsgObj{Msg: responses.SafeToUpgrade})
+
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+	assert.Equal(suite.T(), string(output), recorder.Body.String())
+
+	suite.dbMock.AssertExpectations(suite.T())
+}
