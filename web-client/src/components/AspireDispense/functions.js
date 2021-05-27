e28200d6eea4d989f97918ae96159a1790d50287
Aryan Jain <aryanjn807@gmail.com>
2021-05-27 11:59:43 +0530

    added aspire and dispense heading

--- web-client/src/components/AspireDispense/functions.js
+++ web-client/src/components/AspireDispense/functions.js
@@ -1,3 +1,18 @@
+export const getCategoryLabel = (tabID) => {
+  switch (tabID) {
+    case "1":
+      return "Category 1";
+    case "2":
+      return "Category 2";
+    case "3":
+      return "Shaker";
+    case "4":
+      return "Deck Position";
+    default:
+      return;
+  }
+};
+
 export const getCategoryName = (tabID) => {
   switch (tabID) {
     case "1":
