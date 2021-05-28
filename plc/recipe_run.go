a88fc458d1988d200d4a0ed2f55df85fcd3369d8
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-05-28 21:15:07 +0530

    CheckIfRecipeOrProcessInRun defined for Compact32Deck

--- plc/recipe_run.go
+++ plc/recipe_run.go
@@ -1,11 +1,34 @@
 package plc
 
 import (
+	"github.com/google/uuid"
 	"mylab/cpagent/db"
 	"mylab/cpagent/responses"
 
 	logger "github.com/sirupsen/logrus"
 )
+
+func (d *Compact32Deck) CheckIfRecipeOrProcessSafeForCRUDs(recipeID *uuid.UUID, processID *uuid.UUID) (err error) {
+	if recipeID != nil {
+		if *recipeID == deckRecipe[d.name].ID {
+			logger.Errorln(responses.RecipeUnsafeForCRUDError)
+			return responses.RecipeUnsafeForCRUDError
+		}
+		logger.Infoln(responses.RecipeSafeForCRUD)
+	}
+
+	if processID != nil {
+		for _, process := range deckProcesses[d.name] {
+			if *processID == process.ID {
+				logger.Errorln(responses.ProcessUnsafeForCRUDError)
+				return responses.ProcessUnsafeForCRUDError
+			}
+		}
+		logger.Infoln(responses.ProcessSafeForCRUD)
+	}
+
+	return
+}
 
 func (d *Compact32Deck) RunRecipeWebsocketData(recipe db.Recipe, processes []db.Process) (err error) {
 	deckRecipe[d.name] = recipe
