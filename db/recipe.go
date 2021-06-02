08f1309ed22cd0cb2dc3b95f96332e49a35507f3
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-06-02 10:40:31 +0530

    recipe struct updated for v1.3

--- db/recipe.go
+++ db/recipe.go
@@ -9,13 +9,9 @@ import (
 )
 
 const (
-	getRecipeQuery = `SELECT *
-						FROM recipes
-						WHERE id = $1`
-	selectRecipesQuery = `SELECT *
-						FROM recipes`
-	deleteRecipeQuery = `DELETE FROM recipes
-						WHERE id = $1`
+	getRecipeQuery = `SELECT * FROM recipes WHERE id = $1`
+	selectRecipesQuery = `SELECT * FROM recipes`
+	deleteRecipeQuery = `DELETE FROM recipes WHERE id = $1`
 	createRecipeQuery = `INSERT INTO recipes (
 						name,
 						description,
@@ -24,12 +20,14 @@ const (
 						pos_3,
 						pos_4,
 						pos_5,
-						pos_cartridge_1,
+						pos_6,
 						pos_7,
-						pos_cartridge_2,
+						pos_cartridge_1,
 						pos_9,
+						pos_cartridge_2,
+						pos_11,
 						is_published)
-						VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12) RETURNING id`
+						VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING id`
 	updateRecipeQuery = `UPDATE recipes SET (
 						name,
 						description,
@@ -38,27 +36,31 @@ const (
 						pos_3,
 						pos_4,
 						pos_5,
-						pos_cartridge_1,
+						pos_6,
 						pos_7,
-						pos_cartridge_2,
+						pos_cartridge_1,
 						pos_9,
+						pos_cartridge_2,
+						pos_11,
 						is_published,
-						updated_at) = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) WHERE id = $14`
+						updated_at) = ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15) WHERE id = $16`
 )
 
 type Recipe struct {
 	ID                 uuid.UUID `db:"id" json:"id"`
 	Name               string    `db:"name" json:"name"`
 	Description        string    `db:"description" json:"description"`
-	Position1          int64     `db:"pos_1" json:"pos_1"`
-	Position2          int64     `db:"pos_2" json:"pos_2"`
-	Position3          int64     `db:"pos_3" json:"pos_3"`
-	Position4          int64     `db:"pos_4" json:"pos_4"`
-	Position5          int64     `db:"pos_5" json:"pos_5"`
-	Cartridge1Position int64     `db:"pos_cartridge_1" json:"pos_cartridge_1"`
-	Position7          int64     `db:"pos_7" json:"pos_7"`
-	Cartridge2Position int64     `db:"pos_cartridge_2" json:"pos_cartridge_2"`
-	Position9          int64     `db:"pos_9" json:"pos_9"`
+	Position1          *int64    `db:"pos_1" json:"pos_1"`
+	Position2          *int64    `db:"pos_2" json:"pos_2"`
+	Position3          *int64    `db:"pos_3" json:"pos_3"`
+	Position4          *int64    `db:"pos_4" json:"pos_4"`
+	Position5          *int64    `db:"pos_5" json:"pos_5"`
+	Position6          *int64    `db:"pos_6" json:"pos_6"`
+	Position7          *int64    `db:"pos_7" json:"pos_7"`
+	Cartridge1Position *int64    `db:"pos_cartridge_1" json:"pos_cartridge_1"`
+	Position9          *int64    `db:"pos_9" json:"pos_9"`
+	Cartridge2Position *int64    `db:"pos_cartridge_2" json:"pos_cartridge_2"`
+	Position11         *int64    `db:"pos_11" json:"pos_11"`
 	ProcessCount       int64     `db:"process_count" json:"process_count"`
 	IsPublished        bool      `db:"is_published" json:"is_published"`
 	TotalTime          int64     `db:"total_time" json:"total_time"`
@@ -95,10 +97,12 @@ func (s *pgStore) CreateRecipe(ctx context.Context, r Recipe) (createdRecipe Rec
 		r.Position3,
 		r.Position4,
 		r.Position5,
-		r.Cartridge1Position,
+		r.Position6,
 		r.Position7,
-		r.Cartridge2Position,
+		r.Cartridge1Position,
 		r.Position9,
+		r.Cartridge2Position,
+		r.Position11,
 		r.IsPublished,
 	).Scan(&lastInsertID)
 
@@ -134,10 +138,12 @@ func (s *pgStore) UpdateRecipe(ctx context.Context, r Recipe) (err error) {
 		r.Position3,
 		r.Position4,
 		r.Position5,
-		r.Cartridge1Position,
+		r.Position6,
 		r.Position7,
-		r.Cartridge2Position,
+		r.Cartridge1Position,
 		r.Position9,
+		r.Cartridge2Position,
+		r.Position11,
 		r.IsPublished,
 		time.Now(),
 		r.ID,
