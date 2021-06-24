c1bf061a0461f0ccf04eeb0d7479d54eeb958077
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-06-24 16:44:49 +0530

    run TEC updated so that we don't depend on web-socket for testing

--- service/run_TEC_http.go
+++ service/run_TEC_http.go
@@ -1,8 +1,9 @@
 package service
 
 import (
-	"mylab/cpagent/tec"
 	"net/http"
+	"mylab/cpagent/plc"
+	
 )
 
 func runTECHandler(deps Dependencies) http.HandlerFunc {
@@ -10,7 +11,22 @@ func runTECHandler(deps Dependencies) http.HandlerFunc {
 
 		var err error
 
-		err = tec.Run()
+		p := plc.Stage{
+		Holding: []plc.Step{
+			plc.Step{65.3, 10, 5},
+			plc.Step{85.3, 10, 5},
+			plc.Step{95, 10, 5},
+		},
+		Cycle: []plc.Step{
+			// plc.Step{60, 10, 10},
+			plc.Step{95, 10, 5},
+			plc.Step{85, 10, 5},
+			plc.Step{75, 10, 5},
+			plc.Step{65, 10, 5},
+		},
+		CycleCount: 3,
+	}
+		err = startExp(deps, p)
 		if err != nil{
 			responseCodeAndMsg(rw, http.StatusInternalServerError, ErrObj{Err: err.Error()})
 			return
