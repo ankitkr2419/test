1c81ce22583dfa30589825b7ca37f2b0ae63c278
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-07-07 16:56:15 +0530

    UPDATE, DELETE CASCADES added in migrations. Without these the created experiments won't allow template deletion.

--- migrations/1589525664_create_results_table.up.sql
+++ migrations/1589525664_create_results_table.up.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS results (
   f_value float,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (experiment_id) REFERENCES experiments(id) ON DELETE CASCADE
+  FOREIGN KEY (experiment_id) REFERENCES experiments(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
