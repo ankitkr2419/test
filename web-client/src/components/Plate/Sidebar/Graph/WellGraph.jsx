e1578d0206638da8f202e7c661ada61d1a07544f
Aryan Jain <aryanjn807@gmail.com>
2021-07-10 11:23:04 +0530

    changed font style and size of graphs

--- web-client/src/components/Plate/Sidebar/Graph/WellGraph.jsx
+++ web-client/src/components/Plate/Sidebar/Graph/WellGraph.jsx
@@ -1,62 +1,108 @@
-import React from 'react';
-import { LineChart } from 'core-components';
-import { Text } from 'shared-components';
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import { MIN_THRESHOLD, MAX_THRESHOLD } from 'components/Target/targetConstants';
-import GraphFilters from './GraphFilters';
+import React from "react";
+import { LineChart } from "core-components";
+import { Text } from "shared-components";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import {
+  MIN_THRESHOLD,
+  MAX_THRESHOLD,
+} from "components/Target/targetConstants";
+import GraphFilters from "./GraphFilters";
 
 const options = {
-	legend: {
-		display: false,
-	},
+  legend: {
+    display: false,
+  },
+  scales: {
+    xAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: "Cycles",
+          fontSize: 15,
+          fontStyle: "bold",
+          padding: 5,
+        },
+        offset: true,
+        ticks: {
+          fontSize: 15,
+          fontStyle: "bold",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: "F-value",
+          fontSize: 15,
+          fontStyle: "bold",
+          padding: 10,
+        },
+        ticks: {
+          fontSize: 15,
+          fontStyle: "bold",
+        },
+      },
+    ],
+  },
+  //TODO: will be added later after testing & studying about it properly.
+  // This is for animation.
+
+  // animation: {
+  //   duration: 1000,
+  //   easing: "linear",
+  // },
 };
 
 const WellGraph = ({
-	data,
-	experimentGraphTargetsList,
-	onThresholdChangeHandler,
-	toggleGraphFilterActive,
-	isThresholdInvalid,
-	setThresholdError,
-	resetThresholdError,
+  data,
+  experimentGraphTargetsList,
+  onThresholdChangeHandler,
+  toggleGraphFilterActive,
+  isThresholdInvalid,
+  setThresholdError,
+  resetThresholdError,
 }) => (
-	<div>
-		<GraphCard>
-			<LineChart data={data} options={options}/>
-		</GraphCard>
-		<GraphFilters
-			targets={experimentGraphTargetsList}
-			onThresholdChangeHandler={onThresholdChangeHandler}
-			toggleGraphFilterActive={toggleGraphFilterActive}
-			setThresholdError={setThresholdError}
-			resetThresholdError={resetThresholdError}
-		/>
-		{isThresholdInvalid && (
-			<Text Tag="p" size={14} className='text-danger px-2 mb-1'>
-      	Threshold value should be between {MIN_THRESHOLD} - {MAX_THRESHOLD}
-			</Text>
-		)}
-		<Text size={14} className='text-default mb-0'>
+  <div>
+    <Text size={20} className="text-default mb-4">
+      Amplification Plot
+    </Text>
+    <GraphCard>
+      <LineChart data={data} options={options} />
+    </GraphCard>
+    <GraphFilters
+      targets={experimentGraphTargetsList}
+      onThresholdChangeHandler={onThresholdChangeHandler}
+      toggleGraphFilterActive={toggleGraphFilterActive}
+      setThresholdError={setThresholdError}
+      resetThresholdError={resetThresholdError}
+    />
+    {isThresholdInvalid && (
+      <Text Tag="p" size={14} className="text-danger px-2 mb-1">
+        Threshold value should be between {MIN_THRESHOLD} - {MAX_THRESHOLD}
+      </Text>
+    )}
+    <Text size={14} className="text-default text-center mb-0">
       Note: Click on the threshold number to change it.
-		</Text>
-	</div>
+    </Text>
+  </div>
 );
 
 const GraphCard = styled.div`
-	width: 830px;
-	height: 344px;
-	background: #ffffff 0% 0% no-repeat padding-box;
-	border: 1px solid #707070;
-	padding: 8px;
-	margin: 0 0 32px 0;
+  width: 830px;
+  height: 272px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #707070;
+  padding: 8px;
+  margin: 0 0 32px 0;
 `;
 
 WellGraph.propTypes = {
-	experimentGraphTargetsList: PropTypes.object.isRequired,
-	onThresholdChangeHandler: PropTypes.func.isRequired,
-	toggleGraphFilterActive: PropTypes.func.isRequired,
-	data: PropTypes.object.isRequired,
+  experimentGraphTargetsList: PropTypes.object.isRequired,
+  onThresholdChangeHandler: PropTypes.func.isRequired,
+  toggleGraphFilterActive: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
 };
 
 export default React.memo(WellGraph);
--- web-client/src/containers/TemperatureGraphContainer.jsx
+++ web-client/src/containers/TemperatureGraphContainer.jsx
@@ -1,59 +1,76 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { LineChart } from 'core-components';
-import { Text } from 'shared-components';
-import { getTemperatureChartData } from 'selectors/temperatureGraphSelector';
-import styled from 'styled-components';
+import React from "react";
+import { useSelector } from "react-redux";
+import { LineChart } from "core-components";
+import { Text } from "shared-components";
+import { getTemperatureChartData } from "selectors/temperatureGraphSelector";
+import styled from "styled-components";
 
 const options = {
-	legend: {
-		display: false,
-	},
-	scales: {
-		xAxes: [{
-			scaleLabel: {
-				display: true,
-				labelString: 'minutes',
-			},
-			type: 'linear',
-			ticks: {
-				source: 'data',
-				beginAtZero: true,
-				suggestedMin: 0,
-				min: 0,
-			},
-		}],
-		yAxes: [{
-			scaleLabel: {
-				display: true,
-				labelString: 'Temperature',
-			},
-		}],
-	},
+  legend: {
+    display: false,
+  },
+  scales: {
+    xAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: "Time (minutes)",
+          fontSize: 15,
+          fontStyle: "bold",
+          padding: 10,
+        },
+        offset: true,
+        type: "linear",
+        ticks: {
+          source: "data",
+          beginAtZero: true,
+          suggestedMin: 0,
+          min: 0,
+          fontSize: 15,
+          fontStyle: "bold",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: "Temperature (°C)",
+          fontSize: 15,
+          fontStyle: "bold",
+          padding: 10,
+        },
+        ticks: {
+          fontSize: 15,
+          fontStyle: "bold",
+        },
+      },
+    ],
+  },
 };
 
 const TemperatureGraphContainer = (props) => {
-	// Extracting temperature graph data, Which is populated from websocket
-	const temperatureChartData = useSelector(getTemperatureChartData);
-	return (
-		<div>
-			<Text size={20} className='text-default mb-4'>
-					Temperature Plot
-			</Text>
-			<GraphCard>
-				<LineChart data={temperatureChartData} options={options}/>
-			</GraphCard>
-		</div>
-	);
+  // Extracting temperature graph data, Which is populated from websocket
+  const temperatureChartData = useSelector(getTemperatureChartData);
+  return (
+    <div>
+      <Text size={20} className="text-default mb-4">
+        Temperature Plot
+      </Text>
+      <GraphCard>
+        <LineChart data={temperatureChartData} options={options} />
+      </GraphCard>
+    </div>
+  );
 };
 
 const GraphCard = styled.div`
-	width: 830px;
-	height: 360px;
-	background: #ffffff 0% 0% no-repeat padding-box;
-	border: 1px solid #707070;
-	padding: 8px;
-	margin: 0 0 32px 0;
+  width: 830px;
+  height: 280px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #707070;
+  padding: 8px;
+  margin: 0 0 32px 0;
 `;
 
 export default React.memo(TemperatureGraphContainer);
