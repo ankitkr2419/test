417755aa1002a6565fcf5a6ae2f0c38d5d3220a7
Rupesh Gadekar <rupesh.gadekar@joshsoftware.com>
2021-07-11 21:35:50 +0530

    rtpcr progress bar ui

--- web-client/src/components/Plate/Header/index.jsx
+++ web-client/src/components/Plate/Header/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Text } from "shared-components";
+import { Progress } from "reactstrap";
+import { Icon, Text } from "shared-components";
 import TemplatePopover from "components/Plate/Popover";
 import { formatDate, formatTime } from "utils/helpers";
 
 const StyledSubHeader = styled.div`
   background-color: #f2f2f2;
-  display: flex;
   height: 68px;
-  padding: 12px 16px 12px 76px;
+  padding: 10px 16px 10px 76px;
   color: #707070;
 
   h6 {
@@ -30,24 +30,40 @@ const SubHeader = (props) => {
   const { start_time, end_time, well_count } = experimentDetails.toJS();
 
   return (
-    <StyledSubHeader className="plate-subheader">
-      <Text Tag="h6" className="text-capitalize mb-0">
-        {templateName}
-      </Text>
-      {isExperimentSucceeded === true && (
-        <>
-          <Text Tag="h6" className="mb-0 ml-5">
-            {formatDate(start_time)}
-          </Text>
-          <Text Tag="h6" className="mb-0 ml-3">
-            {`${formatTime(start_time)} to ${formatTime(end_time)}`}
-          </Text>
-          <Text Tag="h6" className="mb-0 ml-5">
-            No. of wells - {well_count}
-          </Text>
-        </>
-      )}
-      <TemplatePopover name={templateName} className="ml-auto" />
+    <StyledSubHeader className="plate-subheader d-flex flex-column">
+      <div className="progress-wrapper d-flex align-items-center mb-auto">
+        <div className="d-flex align-items-center">
+          <Icon size={20} name="timer" className="text-primary" />
+          <div className="time-wrapper d-flex align-items-center">
+            <Text>1 Hr</Text>
+            <div className="separator"></div>
+            <Text>8 min</Text>
+            <Text Tag="span">remaining</Text>
+          </div>
+        </div>
+        <div className="d-flex align-items-center flex-100 ml-3">
+          <Progress value={50} className="experiment-progress w-100" />
+        </div>
+      </div>
+      <div className="d-flex align-items-center">
+        <Text Tag="h6" className="text-capitalize mb-0">
+          {templateName}
+        </Text>
+        {isExperimentSucceeded === true && (
+          <>
+            <Text Tag="h6" className="mb-0 ml-5">
+              {formatDate(start_time)}
+            </Text>
+            <Text Tag="h6" className="mb-0 ml-3">
+              {`${formatTime(start_time)} to ${formatTime(end_time)}`}
+            </Text>
+            <Text Tag="h6" className="mb-0 ml-5">
+              No. of wells - {well_count}
+            </Text>
+          </>
+        )}
+        <TemplatePopover name={templateName} className="ml-auto" />
+      </div>
     </StyledSubHeader>
   );
 };
