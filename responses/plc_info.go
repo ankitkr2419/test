380e616b85a92d092d1c9542f0d6f70c6f18a133
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-07-23 13:24:39 +0530

    Heating and Shaking error and info added

--- responses/plc_info.go
+++ responses/plc_info.go
@@ -4,6 +4,8 @@ var (
 	PIDCalibrationSuccess       = "pid calibration was successful"
 	PIDCalibrationStarted 		= "pid calibration started"
 	PIDCalibrationHeaterStarted = "heater started for pid calibration"
+	ShakingSuccess				= "shaking started success"
+	HeatingSuccess			    = "heating started success"
 	RecipeSafeForCUD         = "recipe is safe for CUDs"
 	ProcessSafeForCUD        = "process is safe for CUDs"
 	SetRunRecipeDataSuccess   = "running recipe's data was successfully stored"
