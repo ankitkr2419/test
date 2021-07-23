cc4b7dc0251706a21ed3140dca2a99bf61d12ca7
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-07-23 14:52:06 +0530

    route changed for starting shaker, heater

--- service/router.go
+++ service/router.go
@@ -148,8 +148,8 @@ func InitRouter(deps Dependencies) (router *mux.Router) {
 	//TODO: allow only for engineer
 	router.HandleFunc("/pid-calibration/{deck:[A-B]}", pidCalibrationHandler(deps)).Methods(http.MethodGet).Headers(versionHeader, v1)
 	// shaker and heater for engineer
-	router.HandleFunc("/shaker/{deck:[A-B]}", shakerHandler(deps)).Methods(http.MethodPost, http.MethodOptions).Headers(versionHeader, v1)
-	router.HandleFunc("/heater/{deck:[A-B]}", heaterHandler(deps)).Methods(http.MethodPost, http.MethodOptions).Headers(versionHeader, v1)
+	router.HandleFunc("/start-shaking/{deck:[A-B]}", shakerHandler(deps)).Methods(http.MethodPost, http.MethodOptions).Headers(versionHeader, v1)
+	router.HandleFunc("/start-heating/{deck:[A-B]}", heaterHandler(deps)).Methods(http.MethodPost, http.MethodOptions).Headers(versionHeader, v1)
 
 	router.HandleFunc("/app-info", appInfoHandler(deps)).Methods(http.MethodGet).Headers(versionHeader, v1)
 
