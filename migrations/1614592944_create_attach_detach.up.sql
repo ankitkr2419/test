3ac1a07f554be35aeacfa13c0fa0006546f3cd3b
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-07-26 14:54:33 +0530

    attach detach migrations updated for magnet height operation

--- migrations/1614592944_create_attach_detach.up.sql
+++ migrations/1614592944_create_attach_detach.up.sql
@@ -1,11 +1,12 @@
 CREATE TYPE magnet_operation_type  AS ENUM ('attach','detach');
-CREATE TYPE magnet_operation_subtype AS ENUM ('lysis', 'wash', 'elusion','full_detach', 'semi_detach');
+-- NOTE: Subtypes are unused after the requirement analysis from Bio team
+-- CREATE TYPE magnet_operation_subtype AS ENUM ('lysis', 'wash', 'elusion','full_detach', 'semi_detach');
 
 
 CREATE TABLE IF NOT EXISTS attach_detach(
  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
  operation magnet_operation_type,
- operation_type magnet_operation_subtype,
+ height int,
  process_id uuid UNIQUE NOT NULL,
  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
