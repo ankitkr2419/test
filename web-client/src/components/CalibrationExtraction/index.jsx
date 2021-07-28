fdd0db9d52fce5863fbfd9b5ab71df2c14fd4fd5
Aryan Jain <aryanjn807@gmail.com>
2021-07-28 15:33:10 +0530

    -> added log out functionality to engineers flow for extraction

--- web-client/src/components/CalibrationExtraction/index.jsx
+++ web-client/src/components/CalibrationExtraction/index.jsx
@@ -11,15 +11,37 @@ import {
   Row,
   Col,
 } from "core-components";
-
+import { ButtonIcon, MlModal } from "shared-components";
+import { MODAL_BTN, MODAL_MESSAGE } from "appConstants";
 const CalibrationExtractionComponent = (props) => {
+  const { deckName, handleLogout, showConfirmationModal, toggleConfirmModal } =
+    props;
   return (
     <div className="calibration-content px-5">
+      <ButtonIcon
+        name="logout"
+        size={28}
+        className="ml-2 bg-white border-primary"
+        onClick={toggleConfirmModal}
+      />
+
       <Card default className="my-5">
         <CardBody className="px-5 py-4">
           <p>Extraction Flow: Calibration</p>
         </CardBody>
       </Card>
+
+      {showConfirmationModal && (
+        <MlModal
+          isOpen={showConfirmationModal}
+          textHead={deckName}
+          textBody={MODAL_MESSAGE.exitConfirmation}
+          successBtn={MODAL_BTN.yes}
+          failureBtn={MODAL_BTN.no}
+          handleSuccessBtn={handleLogout}
+          handleCrossBtn={toggleConfirmModal}
+        />
+      )}
     </div>
   );
 };
--- web-client/src/containers/CalibrationExtractionContainer.jsx
+++ web-client/src/containers/CalibrationExtractionContainer.jsx
@@ -1,17 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import CalibrationExtractionComponent from "components/CalibrationExtraction";
+import { logoutInitiated } from "action-creators/loginActionCreators";
 
 const CalibrationExtractionContainer = () => {
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const [showConfirmationModal, setConfirmModal] = useState(false);
+
   //get login reducer details
   const loginReducer = useSelector((state) => state.loginReducer);
   const loginReducerData = loginReducer.toJS();
   let activeDeckObj = loginReducerData?.decks.find((deck) => deck.isActive);
-  const { token } = activeDeckObj;
+  const { name, token } = activeDeckObj;
+
+  const handleLogout = () => {
+    dispatch(
+      logoutInitiated({ deckName: name, token: token, showToast: true })
+    );
+  };
+
+  const toggleConfirmModal = () => setConfirmModal(!showConfirmationModal);
 
   //api call to get configurations
   // useEffect(() => {
@@ -20,7 +31,14 @@ const CalibrationExtractionContainer = () => {
   //   }
   // }, [dispatch, token]);
 
-  return <CalibrationExtractionComponent />;
+  return (
+    <CalibrationExtractionComponent
+      toggleConfirmModal={toggleConfirmModal}
+      handleLogout={handleLogout}
+      showConfirmationModal={showConfirmationModal}
+      deckName={name}
+    />
+  );
 };
 
 export default React.memo(CalibrationExtractionContainer);
