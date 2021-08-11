7eda1bf73b2a4f5fbcc164752458d8d881a39bf2
Ankush Kadam <ankush.kadam@joshsoftware.com>
2021-08-11 12:03:17 +0530

    experiment details added in report

--- web-client/src/components/modals/PreviewReportModal/index.jsx
+++ web-client/src/components/modals/PreviewReportModal/index.jsx
@@ -41,7 +41,6 @@ const PreviewReportModal = (props) => {
   //also synce local loading with save report api isLoading when report is generated
   const [isReportGenerated, setIsReportGenerated] = useState(false);
 
-
   //sync local loading with api isLoading when report generated
   useEffect(() => {
     if (isReportGenerated === true) {
@@ -58,42 +57,51 @@ const PreviewReportModal = (props) => {
     //access html element we want to export contents from
     let page1 = document.getElementById("page-1");
     let page2 = document.getElementById("page-2");
-    // let page3 = document.getElementById("page-3");
+    let page3 = document.getElementById("page-3");
 
     //convert html element into canvas
     html2canvas(page1).then((canvas1) => {
       html2canvas(page2).then((canvas2) => {
-        // html2canvas(page3).then((canvas3) => {
-        const img1 = canvas1.toDataURL("image/png");
-        const img2 = canvas2.toDataURL("image/png");
-        // const img3 = canvas3.toDataURL("image/png");
-        const doc = new jsPDF("l", "pt", [1024, 700]); //create custom pdf instance with its properties [width, height] in pt units //NOTE: (1 pt = 1.3281472327365 px)
-        //add contents into pdf
-        doc.addImage(
-          img1,
-          "png",
-          30, //page1.offsetLeft,
-          100, //page1.offsetTop,
-          page1.clientWidth,
-          page1.clientHeight
-        );
-        doc.addPage();
-        doc.addImage(
-          img2,
-          "png",
-          30, //page2.offsetLeft,//left
-          100, //page2.offsetTop//top
-          page2.clientWidth,
-          page2.clientHeight
-        );
-        // doc.save("chart.pdf"); //save file locally with default filename //kept for testing
-        setIsReportGenerated(true);
-        let pdfInBlobFormat = doc.output("blob"); //generate blob file
+        html2canvas(page3).then((canvas3) => {
+          const img1 = canvas1.toDataURL("image/png");
+          const img2 = canvas2.toDataURL("image/png");
+          const img3 = canvas3.toDataURL("image/png");
+          const doc = new jsPDF("l", "pt", [1024, 700]); //create custom pdf instance with its properties [width, height] in pt units //NOTE: (1 pt = 1.3281472327365 px)
+          //add contents into pdf
+          doc.addImage(
+            img1,
+            "png",
+            30, //page1.offsetLeft,
+            100, //page1.offsetTop,
+            page1.clientWidth + 100,
+            page1.clientHeight
+          );
+          doc.addPage();
+          doc.addImage(
+            img3,
+            "png",
+            30,
+            100,
+            page3.clientWidth,
+            page3.clientHeight
+          );
+          doc.addPage();
+          doc.addImage(
+            img2,
+            "png",
+            30,
+            100,
+            page2.clientWidth,
+            page2.clientHeight
+          );
 
-        //save to server
-        sendReportToServer(pdfInBlobFormat);
+          // doc.save("chart.pdf"); //save file locally with default filename //kept for testing
+          setIsReportGenerated(true);
+          let pdfInBlobFormat = doc.output("blob"); //generate blob file
 
-        // }); //page 3 finish
+          //save to server
+          sendReportToServer(pdfInBlobFormat);
+        }); //page 3 finish
       }); //page 2 finish
     }); //page 1 finish
   };
@@ -112,15 +120,13 @@ const PreviewReportModal = (props) => {
   const showPageOne = () => {
     return (
       <div id="page-1">
-        {/** amplification graph */}
-        <Text className="font-weight-bold text-center mb-4">Amplification</Text>
-        <ExperimentGraphContainer
-          showTempGraph={false}
+        <Header
+          data={data}
+          experimentTemplate={experimentTemplate}
           experimentStatus={experimentStatus}
-          isSidebarOpen={isSidebarOpen}
-          setIsSidebarOpen={setIsSidebarOpen}
-          resetSelectedWells={resetSelectedWells}
-          isMultiSelectionOptionOn={isMultiSelectionOptionOn}
+          experimentDetails={experimentDetails}
+          experimentId={experimentId}
+          temperatureData={temperatureData}
         />
       </div>
     );
@@ -143,28 +149,27 @@ const PreviewReportModal = (props) => {
     );
   };
 
-  {
-    /*TODO remove if not needed*/
-  }
-  // const showPageThree = () => {
-  //   return (
-  //     <div id="page-3">
-  //       <Header
-  //         data={data}
-  //         experimentTemplate={experimentTemplate}
-  //         experimentStatus={experimentStatus}
-  //         experimentDetails={experimentDetails}
-  //         experimentId={experimentId}
-  //         temperatureData={temperatureData}
-  //       />
-  //     </div>
-  //   );
-  // };
+  const showPageThree = () => {
+    return (
+      <div id="page-3">
+        {/** amplification graph */}
+        <Text className="font-weight-bold text-center mb-4">Amplification</Text>
+        <ExperimentGraphContainer
+          showTempGraph={false}
+          experimentStatus={experimentStatus}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+          resetSelectedWells={resetSelectedWells}
+          isMultiSelectionOptionOn={isMultiSelectionOptionOn}
+        />
+      </div>
+    );
+  };
 
   return (
     <Modal isOpen={isOpen} centered size="lg">
       <ModalBody>
-        <div className="d-flex mt-5">
+        <div className="d-flex mt-5 mb-3">
           <Text
             Tag="h4"
             size={24}
@@ -196,15 +201,14 @@ const PreviewReportModal = (props) => {
         )}
 
         {/** View Report */}
+        {showPageOne()}
         <div
           className="d-flex flex-column align-items-center export-contents"
           //adjust scale to fit graphs on modal width
-          //& disable user interactions
-          style={{ transform: "scale(0.8)", pointerEvents: "none" }}
+          style={{ transform: "scale(0.8)" }}
         >
-          {showPageOne()}
+          {showPageThree()}
           {showPageTwo()}
-          {/* {showPageThree()} */}
         </div>
       </ModalBody>
     </Modal>
