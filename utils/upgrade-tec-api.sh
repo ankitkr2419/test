c9d19ba9f2b0af21e8255d3b0edb86f28533ae80
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-08-14 13:20:10 +0530

    paths used as variable. Bug related to sed corrected. version specific directory name is taken by script. TODO added.

--- utils/upgrade-tec-api.sh
+++ utils/upgrade-tec-api.sh
@@ -1,27 +1,51 @@
-#!/bin/bash
+#!/bin/bash -xv
+
+# set current and temp path
+current='utils/MeComAPIs/current'
+temp='utils/MeComAPIs/temp'
+
+mkdir ${current} ${temp}
 
 # copy ComPort files to MeComAPI/temp
-cp utils/MeComAPIs/ComPort/* utils/MeComAPIs/temp
+cp utils/MeComAPIs/ComPort/* ${temp}
 
 # copy ConsoleIO files to MeComAPI/temp
-cp utils/MeComAPIs/ConsoleIO/* utils/MeComAPIs/temp
+cp utils/MeComAPIs/ConsoleIO/* ${temp}
 
 # copy TEC files to MeComAPI/temp
-cp utils/MeComAPIs/TEC/* utils/MeComAPIs/temp
+cp utils/MeComAPIs/TEC/* ${temp}
+
+# Let user set zip path
+# default path provided
+read -e -p "please input the MeComAPI zip's Path: " -i "utils/MeComAPIs/MeComAPI_v0.42/MeComAPI_v0.42.zip" zippath
 
 # unzip the MeComAPI
-# TODO: Let user set zip path
-unzip -d utils/MeComAPIs/current/ utils/MeComAPIs/MeComAPI_v0.42/MeComAPI_v0.42.zip
+unzip  ${zippath} -d ${current}
+
+# remove version specific directory names
+parent=$(ls ${current})
+
+cp -r ${current}/${parent}/* ${current}
+
+cp ${current}/MeComAPI/private/* ${current}/MeComAPI
 
 # Delete MePort_Winc.c
-# TODO: remove version specific directory names
-rm utils/MeComAPIs/current/MeComAPI_v0.42/MeComAPI/MePort_Win.c
+rm ${current}/MeComAPI/MePort_Win.c
 
 # Rename MePort_Linux.c to just MePort.c
-mv utils/MeComAPIs/current/MeComAPI_v0.42/MeComAPI/MePort_Linux.c utils/MeComAPIs/current/MeComAPI_v0.42/MeComAPI/MePort.c
+mv ${current}/MeComAPI/MePort_Linux.c ${current}/MeComAPI/MePort.c
 
 # copy MeComAPI folder/subfolders files to MeComAPI/temp
-find utils/MeComAPIs/current/MeComAPI_v0.42/MeComAPI/ -name "*.c" -o -name "*.h" -exec cp  '{}' "utils/MeComAPIs/temp/" ";"
+find ${current}/MeComAPI/ -type f \( -name "*.c" -o -name "*.h" \) -exec cp '{}' "utils/MeComAPIs/temp/" ";"
+
+# change include path of Frame.h to current path
+# NOTE: replacing early with sed didn't work. Changes didn't reflect in copied files
+##TODO: Take suggestion from @krushna to see if there is a better way
+sed -i 's/\.\.\/MePort\.h/MePort\.h/g' ${temp}/MeFrame.h
+sed -i 's/\.\.\/MePort\.h/MePort\.h/g' ${temp}/MeFrame.c
+sed -i 's/private\/MeFrame\.h/MeFrame\.h/g' ${temp}/MePort.c
+sed -i 's/\.\.\/ComPort\/ComPort.h/ComPort\.h/g' ${temp}/MePort.c
+sed -i 's/\.\.\/MeCom\.h/MeCom\.h/g' ${temp}/MeCom.c
 
 # change include path properly to include all the files under single directory i.e under temp
 # Reason: go considers all of C files as a single package
@@ -35,10 +59,10 @@ find utils/MeComAPIs/current/MeComAPI_v0.42/MeComAPI/ -name "*.c" -o -name "*.h"
 # For Windows the ports will be like /dev/S4, this means ComPortNr value is 4
 
 # copy the files in MeComAPI/temp folder to tec_1089. Overwriting if needed.
-cp -rf utils/MeComAPIs/temp/* tec/tec_1089/
+cp -rf ${temp}/* tec/tec_1089/
 
 # Clear current and temp directory
-rm -r utils/MeComAPIs/current utils/MeComAPIs/temp
+rm -r ${current} ${temp}
 
 # If any old files are causing trouble while building, then consider deleting/modifying them.
 
