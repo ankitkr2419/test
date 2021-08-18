22db3214c8f772542b9ecbed8a57888398943830
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-08-18 13:19:52 +0530

    TEC struct created for TEC Eng config. Set and Get mothods defined accordingly

--- config/tec.go
+++ config/tec.go
@@ -5,6 +5,15 @@ import (
 	"github.com/spf13/viper"
 )
 
+type TEC struct {
+	CurrentLimitation       float64 `json:"current_limitation"`
+	VoltageLimitation       float64 `json:"voltage_limitation"`
+	CurrentErrorThreshold   float64 `json:"current_error_threshold"`
+	VoltageErrorThreshold   float64 `json:"voltage_error_threshold"`
+	PeltierMaxCurrent       float64 `json:"peltier_max_current"`
+	PeltierDeltaTemperature float64 `json:"peltier_delta_temperature"`
+}
+
 func GetCurrentLimitation() float64 {
 	return ReadEnvFloat("CurrentLimitation")
 }
@@ -53,33 +62,16 @@ func SetPeltierDeltaTemperature(value float64) {
 	viper.Set("PeltierDeltaTemperature", value)
 }
 
-type TEC struct {
-	CurrentLimitation       float64 `json:"current_limitation"`
-	VoltageLimitation       float64 `json:"voltage_limitation"`
-	CurrentErrorThreshold   float64 `json:"current_error_threshold"`
-	VoltageErrorThreshold   float64 `json:"voltage_error_threshold"`
-	PeltierMaxCurrent       float64 `json:"peltier_max_current"`
-	PeltierDeltaTemperature float64 `json:"peltier_delta_temperature"`
-}
-
-func SetTECValues(tec TEC) (err error) {
-
-	cl := GetCurrentLimitation()
-	vl := GetVoltageLimitation()
-	cet := GetCurrentErrorThreshold()
-	vet := GetVoltageErrorThreshold()
-	pmc := GetPeltierMaxCurrent()
-	pdt := GetPeltierDeltaTemperature()
+func SetTECConfigValues(tec TEC) (err error) {
 
 	oldString, newString = []string{}, []string{}
 	oldString = append(oldString,
-
-		fmt.Sprintf("CurrentLimitation : %.2f", cl),
-		fmt.Sprintf("VoltageLimitation : %.2f", vl),
-		fmt.Sprintf("CurrentErrorThreshold: %.2f", cet),
-		fmt.Sprintf("VoltageErrorThreshold: %.2f", vet),
-		fmt.Sprintf("PeltierMaxCurrent: %.2f", pmc),
-		fmt.Sprintf("PeltierDeltaTemperature: %.2f", pdt),
+		fmt.Sprintf("CurrentLimitation : %.2f", GetCurrentLimitation()),
+		fmt.Sprintf("VoltageLimitation : %.2f", GetVoltageLimitation()),
+		fmt.Sprintf("CurrentErrorThreshold: %.2f", GetCurrentErrorThreshold()),
+		fmt.Sprintf("VoltageErrorThreshold: %.2f", GetVoltageErrorThreshold()),
+		fmt.Sprintf("PeltierMaxCurrent: %.2f", GetPeltierMaxCurrent()),
+		fmt.Sprintf("PeltierDeltaTemperature: %.2f", GetPeltierDeltaTemperature()),
 	)
 	newString = append(newString,
 		fmt.Sprintf("CurrentLimitation : %.2f", tec.CurrentLimitation),
@@ -90,7 +82,7 @@ func SetTECValues(tec TEC) (err error) {
 		fmt.Sprintf("PeltierDeltaTemperature: %.2f", tec.PeltierDeltaTemperature),
 	)
 
-	err = UpdateConfig(configPath, oldString, newString)
+	err = UpdateConfig(configPath)
 	if err != nil {
 		return
 	}
@@ -102,4 +94,15 @@ func SetTECValues(tec TEC) (err error) {
 	SetPeltierMaxCurrent(tec.PeltierMaxCurrent)
 	SetPeltierDeltaTemperature(tec.PeltierDeltaTemperature)
 	return
+}
+
+func GetTECConfigValues() TEC {
+	return TEC{
+		CurrentLimitation: GetCurrentLimitation(),
+		VoltageLimitation:  GetVoltageLimitation(),
+		CurrentErrorThreshold: GetCurrentErrorThreshold(),
+		VoltageErrorThreshold: GetVoltageErrorThreshold(),
+		PeltierMaxCurrent: GetPeltierMaxCurrent(),
+		PeltierDeltaTemperature: GetPeltierDeltaTemperature(),
+	}
 }
