f182a4c14f918c3d3ff5a0d72c4f8dd74eeac4cb
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-08-23 17:35:41 +0530

    LidPIDTemp set and get configs added

--- config/rt-pcr.go
+++ config/rt-pcr.go
@@ -9,6 +9,7 @@ type RTPCR struct {
 	HomingTime      int64  `json:"homing_time" validate:"required,lte=30,gte=16"`
 	NumHomingCycles int64  `json:"no_of_homing_cycles" validate:"required,lte=100,gte=0"`
 	CycleTime       int64  `json:"cycle_time" validate:"required,lte=30,gte=2"`
+	LidPIDTemp      int64  `json:"pid_lid_temp" validate:"required,lte=110,gte=50"`
 }
 
 func SetRTPCRConfigValues(rt RTPCR) (err error) {
@@ -18,11 +19,13 @@ func SetRTPCRConfigValues(rt RTPCR) (err error) {
 		fmt.Sprintf("homing_time: %d", GetHomingTime()),
 		fmt.Sprintf("num_homing_cycles: %d", GetNumHomingCycles()),
 		fmt.Sprintf("cycle_time: %d", int64(GetCycleTime())),
+		fmt.Sprintf("pid_lid_temp: %d", int64(GetLidPIDTemp())),
 	)
 	newString = append(newString,
 		fmt.Sprintf("homing_time: %d", rt.HomingTime),
 		fmt.Sprintf("num_homing_cycles: %d", rt.NumHomingCycles),
 		fmt.Sprintf("cycle_time: %d", rt.CycleTime),
+		fmt.Sprintf("pid_lid_temp: %d", rt.LidPIDTemp),
 	)
 
 	err = UpdateConfig(configPath)
@@ -33,6 +36,7 @@ func SetRTPCRConfigValues(rt RTPCR) (err error) {
 	SetHomingTime(rt.HomingTime)
 	SetNumHomingCycles(rt.NumHomingCycles)
 	SetCycleTime(rt.CycleTime)
+	SetLidPIDTemp(rt.LidPIDTemp)
 	return
 }
 
@@ -85,5 +89,14 @@ func GetRTPCRConfigValues() RTPCR {
 		HomingTime: int64(GetHomingTime()),
 		NumHomingCycles: int64(GetNumHomingCycles()),
 		CycleTime: int64(GetCycleTime()),
+		LidPIDTemp: int64(GetLidPIDTemp()),
 	}
+}
+
+func GetLidPIDTemp() int64 {
+	return int64(ReadEnvInt("pid_lid_temp"))
+}
+
+func SetLidPIDTemp(pT int64) {
+	viper.Set("pid_lid_temp", pT)
 }
