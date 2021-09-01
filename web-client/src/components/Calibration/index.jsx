2bedb378f17b8dd1cc2236d867eca3e6229e016d
Ankush Kadam <ankush.kadam@joshsoftware.com>
2021-09-01 10:25:38 +0530

    engineer flow for rtpcr changed structure

--- web-client/src/components/Calibration/index.jsx
+++ web-client/src/components/Calibration/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useCallback } from "react";
-import { useFormik } from "formik";
+import { useHistory } from "react-router";
 
 import {
   Button,
@@ -12,7 +12,7 @@ import {
   Row,
   Col,
 } from "core-components";
-import { Text } from "shared-components";
+import { Icon, Text } from "shared-components";
 
 import {
   isValueValid,
@@ -21,111 +21,39 @@ import {
   getRequestBody,
 } from "./helper";
 
+import { HeadingTitle } from "components/CalibrationExtraction/HeadingTitle";
+import CommonFieldsComponent from "components/CalibrationExtraction/CommonFieldsComponent";
+
 const CalibrationComponent = (props) => {
-  let { configs, saveButtonClickHandler } = props;
+  let { handleOnChange, handleSaveDetailsBtn, formik, isAdmin } = props;
+  const history = useHistory();
 
-  const formik = useFormik({
-    initialValues: formikInitialState,
-    enableReinitialize: true,
-  });
-
-  //store new data in local state
-  useEffect(() => {
-    if (configs) {
-      Object.keys(formik.values).map((element) => {
-        const { apiKey, name } = formik.values[element];
-
-        const newValue = configs[apiKey] ? configs[apiKey] : "";
-        const isValid = isValueValid(name, newValue);
-
-        // set formik fields
-        formik.setFieldValue(`${name}.isInvalid`, !isValid);
-        formik.setFieldValue(`${name}.value`, newValue);
-      });
-    }
-  }, [configs]);
-
-  //validations and api call
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    if (validateAllFields(formik.values) === true) {
-      const requestBody = getRequestBody(formik.values);
-      saveButtonClickHandler(requestBody);
-    }
-  };
-
-  const handleBlurChange = useCallback((name, value) => {
-    const isValid = isValueValid(name, value);
-    formik.setFieldValue(`${name}.isInvalid`, !isValid);
-  }, []);
-
-  const handleOnChange = (event, name) => {
-    formik.setFieldValue(`${name}.value`, event.target.value);
-  };
-
-  const handleOnFocus = (name) => {
-    formik.setFieldValue(`${name}.isInvalid`, false);
+  const handleBack = () => {
+    history.goBack();
   };
 
   return (
     <div className="calibration-content px-5">
+      <div className="d-flex align-items-center">
+        {isAdmin && (
+          <div style={{ cursor: "pointer" }} onClick={handleBack}>
+            <Icon name="angle-left" size={32} className="text-white" />
+            <HeadingTitle
+              Tag="h5"
+              className="text-white font-weight-bold ml-3 mb-0"
+            >
+              Go back to template page
+            </HeadingTitle>
+          </div>
+        )}
+      </div>
       <Card default className="my-5">
         <CardBody className="px-5 py-4">
-          <Form onSubmit={onSubmit}>
-            <Row>
-              {Object.keys(formik.values).map((key, index) => {
-                const element = formik.values[key];
-                const {
-                  type,
-                  name,
-                  label,
-                  min,
-                  max,
-                  value,
-                  isInvalid,
-                  isInvalidMsg,
-                } = element;
-
-                return (
-                  <Col md={6}>
-                    <FormGroup>
-                      <Label for="username">{label}</Label>
-                      <Input
-                        type={type}
-                        name={name}
-                        id={name}
-                        placeholder={
-                          type === "number" ? `${min} - ${max}` : "Type here"
-                        }
-                        value={value}
-                        onChange={(event) => handleOnChange(event, name)}
-                        onBlur={(event) =>
-                          handleBlurChange(name, event.target.value)
-                        }
-                        onFocus={() => handleOnFocus(name)}
-                      />
-                      {(isInvalid || value == null) && (
-                        <div className="flex-70">
-                          <Text Tag="p" size={14} className="text-danger">
-                            {`${isInvalidMsg}`}
-                          </Text>
-                        </div>
-                      )}
-                    </FormGroup>
-                  </Col>
-                );
-              })}
-            </Row>
-            <div className="text-right pt-4 pb-1 mb-3">
-              <Button
-                color="primary"
-                disabled={!validateAllFields(formik.values)}
-              >
-                Save
-              </Button>
-            </div>
-          </Form>
+          {/* Common fields - name, email, room temperature */}
+          <CommonFieldsComponent
+            formik={formik}
+            handleSaveDetailsBtn={handleSaveDetailsBtn}
+          />
         </CardBody>
       </Card>
     </div>
@@ -133,3 +61,142 @@ const CalibrationComponent = (props) => {
 };
 
 export default React.memo(CalibrationComponent);
+
+//old code for references
+//TODO remove this if not needed for reference
+
+// import React, { useEffect, useCallback } from "react";
+// import { useFormik } from "formik";
+
+// import {
+//   Button,
+//   Form,
+//   FormGroup,
+//   Input,
+//   Label,
+//   Card,
+//   CardBody,
+//   Row,
+//   Col,
+// } from "core-components";
+// import { Text } from "shared-components";
+
+// import {
+//   isValueValid,
+//   formikInitialState,
+//   validateAllFields,
+//   getRequestBody,
+// } from "./helper";
+
+// const CalibrationComponent = (props) => {
+//   let { configs, saveButtonClickHandler } = props;
+
+//   const formik = useFormik({
+//     initialValues: formikInitialState,
+//     enableReinitialize: true,
+//   });
+
+//   //store new data in local state
+//   useEffect(() => {
+//     if (configs) {
+//       Object.keys(formik.values).map((element) => {
+//         const { apiKey, name } = formik.values[element];
+
+//         const newValue = configs[apiKey] ? configs[apiKey] : "";
+//         const isValid = isValueValid(name, newValue);
+
+//         // set formik fields
+//         formik.setFieldValue(`${name}.isInvalid`, !isValid);
+//         formik.setFieldValue(`${name}.value`, newValue);
+//       });
+//     }
+//   }, [configs]);
+
+//   //validations and api call
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (validateAllFields(formik.values) === true) {
+//       const requestBody = getRequestBody(formik.values);
+//       saveButtonClickHandler(requestBody);
+//     }
+//   };
+
+//   const handleBlurChange = useCallback((name, value) => {
+//     const isValid = isValueValid(name, value);
+//     formik.setFieldValue(`${name}.isInvalid`, !isValid);
+//   }, []);
+
+//   const handleOnChange = (event, name) => {
+//     formik.setFieldValue(`${name}.value`, event.target.value);
+//   };
+
+//   const handleOnFocus = (name) => {
+//     formik.setFieldValue(`${name}.isInvalid`, false);
+//   };
+
+//   return (
+//     <div className="calibration-content px-5">
+//       <Card default className="my-5">
+//         <CardBody className="px-5 py-4">
+//           <Form onSubmit={onSubmit}>
+//             <Row>
+//               {Object.keys(formik.values).map((key, index) => {
+//                 const element = formik.values[key];
+//                 const {
+//                   type,
+//                   name,
+//                   label,
+//                   min,
+//                   max,
+//                   value,
+//                   isInvalid,
+//                   isInvalidMsg,
+//                 } = element;
+
+//                 return (
+//                   <Col md={6}>
+//                     <FormGroup>
+//                       <Label for="username">{label}</Label>
+//                       <Input
+//                         type={type}
+//                         name={name}
+//                         id={name}
+//                         placeholder={
+//                           type === "number" ? `${min} - ${max}` : "Type here"
+//                         }
+//                         value={value}
+//                         onChange={(event) => handleOnChange(event, name)}
+//                         onBlur={(event) =>
+//                           handleBlurChange(name, event.target.value)
+//                         }
+//                         onFocus={() => handleOnFocus(name)}
+//                       />
+//                       {(isInvalid || value == null) && (
+//                         <div className="flex-70">
+//                           <Text Tag="p" size={14} className="text-danger">
+//                             {`${isInvalidMsg}`}
+//                           </Text>
+//                         </div>
+//                       )}
+//                     </FormGroup>
+//                   </Col>
+//                 );
+//               })}
+//             </Row>
+//             <div className="text-right pt-4 pb-1 mb-3">
+//               <Button
+//                 color="primary"
+//                 disabled={!validateAllFields(formik.values)}
+//               >
+//                 Save
+//               </Button>
+//             </div>
+//           </Form>
+//         </CardBody>
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default React.memo(CalibrationComponent);
--- web-client/src/containers/CalibrationRtpcrContainer.jsx
+++ web-client/src/containers/CalibrationRtpcrContainer.jsx
@@ -1,65 +1,171 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+
 import {
   calibrationInitiated,
   updateCalibrationInitiated,
+  commonDetailsInitiated,
+  updateCommonDetailsInitiated,
 } from "action-creators/calibrationActionCreators";
 import CalibrationComponent from "components/Calibration";
+import { formikInitialState } from "components/Calibration/helper";
+import {
+  deckBlockInitiated,
+  logoutInitiated,
+} from "action-creators/loginActionCreators";
 
 const CalibrationRtpcrContainer = () => {
   const dispatch = useDispatch();
+
+  //formik state for common fields
+  const formik = useFormik({
+    initialValues: formikInitialState,
+    enableReinitialize: true,
+  });
 
   //get login reducer details
   const loginReducer = useSelector((state) => state.loginReducer);
   const loginReducerData = loginReducer.toJS();
   let activeDeckObj = loginReducerData?.decks.find((deck) => deck.isActive);
-  const { token } = activeDeckObj;
+  const { isAdmin, name, token } = activeDeckObj;
 
-  //get calibration configurations from reducer
-  const calibrationReducer = useSelector((state) => state.calibrationReducer);
-  const calibrationReducerData = calibrationReducer.toJS();
-  const { configs } = calibrationReducerData;
-
-  //get calibration configurations from reducer
-  const updateCalibrationReducer = useSelector((state) => state.updateCalibrationReducer);
-  const updateCalibrationReducerData = updateCalibrationReducer.toJS();
-  const { isLoading, error } = updateCalibrationReducerData;
+  const commonDetailsReducer = useSelector(
+    (state) => state.commonDetailsReducer
+  );
+  const commonDetailsReducerData = commonDetailsReducer.toJS();
+  const { isUpdateApi, details } = commonDetailsReducerData;
 
   //initially populate with previous data
   useEffect(() => {
     if (token) {
-      dispatch(calibrationInitiated(token)); 
+      // fetch commonDetails (name, email, roomTemp) API called initially
+      dispatch(commonDetailsInitiated(token));
+      //another deck must be blocked
+      dispatch(deckBlockInitiated({ deckName: name }));
     }
-  }, [dispatch, token])
+  }, [dispatch, token]);
 
-  //after update API call is successful populate fields with new data
   useEffect(() => {
-    if (token && error === false && isLoading === false) {
-      dispatch(calibrationInitiated(token));
+    if (
+      commonDetailsReducerData.error === false &&
+      commonDetailsReducerData.isLoading === false
+    ) {
+      // populate formik data with fetched values
+      if (isUpdateApi === false) {
+        const { receiver_name, receiver_email, room_temperature } = details;
+        formik.setFieldValue("name.value", receiver_name);
+        formik.setFieldValue("email.value", receiver_email);
+        formik.setFieldValue("roomTemperature.value", room_temperature);
+      } else {
+        // fetch updated data after updation
+        dispatch(commonDetailsInitiated(token));
+      }
     }
-  }, [dispatch, isLoading, error, token]);
+  }, [
+    commonDetailsReducerData.error,
+    commonDetailsReducerData.isLoading,
+    isUpdateApi,
+  ]);
 
-  const saveButtonClickHandler = (configData) => {
-    let data = {
-      receiver_name: configData.name,
-      receiver_email: configData.email,
-      room_temperature: configData.roomTemperature,
-      homing_time: configData.homingTime,
-      no_of_homing_cycles: configData.noOfHomingCycles,
-      cycle_time: configData.cycleTime,
-      pid_temperature: configData.pidTemperature,
-      pid_minutes: configData.pidMinutes,
+  const handleSaveDetailsBtn = (data) => {
+    const { name, email, roomTemperature } = data;
+    const requestBody = {
+      receiver_name: name.value,
+      receiver_email: email.value,
+      room_temperature: roomTemperature.value,
     };
+    dispatch(updateCommonDetailsInitiated({ token, data: requestBody }));
+  };
 
-    dispatch(updateCalibrationInitiated({ token, data }));
+  /**to change formik field */
+  const handleOnChange = (key, value) => {
+    formik.setFieldValue(key, value);
   };
 
   return (
     <CalibrationComponent
-      configs={configs}
-      saveButtonClickHandler={saveButtonClickHandler}
+      formik={formik}
+      isAdmin={isAdmin}
+      handleOnChange={handleOnChange}
+      handleSaveDetailsBtn={handleSaveDetailsBtn}
     />
   );
 };
 
 export default React.memo(CalibrationRtpcrContainer);
+
+//old code for reference
+//TODO remove this if not needed for reference
+
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import {
+//   calibrationInitiated,
+//   updateCalibrationInitiated,
+//   commonDetailsInitiated,
+// } from "action-creators/calibrationActionCreators";
+// import CalibrationComponent from "components/Calibration";
+
+// const CalibrationRtpcrContainer = () => {
+//   const dispatch = useDispatch();
+
+//   //get login reducer details
+//   const loginReducer = useSelector((state) => state.loginReducer);
+//   const loginReducerData = loginReducer.toJS();
+//   let activeDeckObj = loginReducerData?.decks.find((deck) => deck.isActive);
+//   const { token } = activeDeckObj;
+
+//   //get calibration configurations from reducer
+//   const calibrationReducer = useSelector((state) => state.calibrationReducer);
+//   const calibrationReducerData = calibrationReducer.toJS();
+//   const { configs } = calibrationReducerData;
+
+//   //get calibration configurations from reducer
+//   const updateCalibrationReducer = useSelector(
+//     (state) => state.updateCalibrationReducer
+//   );
+//   const updateCalibrationReducerData = updateCalibrationReducer.toJS();
+//   const { isLoading, error } = updateCalibrationReducerData;
+
+//   //initially populate with previous data
+//   useEffect(() => {
+//     if (token) {
+//       // dispatch(calibrationInitiated(token));
+
+//       // fetch commonDetails (name, email, roomTemp) API called initially
+//       dispatch(commonDetailsInitiated(token));
+//     }
+//   }, [dispatch, token]);
+
+//   //after update API call is successful populate fields with new data
+//   useEffect(() => {
+//     if (token && error === false && isLoading === false) {
+//       dispatch(calibrationInitiated(token));
+//     }
+//   }, [dispatch, isLoading, error, token]);
+
+//   const saveButtonClickHandler = (configData) => {
+//     let data = {
+//       receiver_name: configData.name,
+//       receiver_email: configData.email,
+//       room_temperature: configData.roomTemperature,
+//       homing_time: configData.homingTime,
+//       no_of_homing_cycles: configData.noOfHomingCycles,
+//       cycle_time: configData.cycleTime,
+//       pid_temperature: configData.pidTemperature,
+//       pid_minutes: configData.pidMinutes,
+//     };
+
+//     dispatch(updateCalibrationInitiated({ token, data }));
+//   };
+
+//   return (
+//     <CalibrationComponent
+//       configs={configs}
+//       saveButtonClickHandler={saveButtonClickHandler}
+//     />
+//   );
+// };
+
+// export default React.memo(CalibrationRtpcrContainer);
