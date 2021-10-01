68ed64ef9ae219fb2d35abb19c4d63ca71edef38
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-10-01 17:05:23 +0530

    run script updated to include application type and plc type

--- run.sh
+++ run.sh
@@ -1,4 +1,30 @@
-#!/bin/bash	
+#!/bin/bash
+export apptype=${1}
+export dev=${2}
+export delay=" --delay 10 "
+
+# Delay is not allowed for compact
+# By default compact32 will start
+if [[ ${dev} == "" ]]; then
+    dev="compact32"
+    delay=""
+elif [[  ${dev} != "simulator" ]]; then
+    echo "Please specify plc type as simulator"
+    exit 1
+fi
+
 chmod +x ./cpagent
 var="./utils/logs/output_$(date +%s).log"
-sudo ./cpagent start --plc simulator --no-rtpcr --delay 1 > ${var}
+
+if [[ ${apptype} == "extraction" ]]; then
+    echo "Logs for current run are present in "${var}
+    sudo ./cpagent start --plc ${dev} --no-rtpcr ${delay} > ${var}
+elif [[ ${apptype} == "rtpcr" ]]; then
+    echo "Logs for current run are present in "${var}
+    sudo ./cpagent start --plc ${dev} --tec ${dev} --no-extraction > ${var}
+else
+  echo "Please specify apptype[extraction, rtpcr]"
+  exit 1
+fi
+
+echo "Logs for current run are present in "${var}
